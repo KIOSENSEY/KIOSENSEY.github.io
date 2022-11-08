04668dfc093ccfb11b8532e0f4c688fe4ec9b5c6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-08 15:14:06 +0300

    Add files via upload

---
+++ files/ps3/js/Call_of_Duty_Classic.js
@@ -0,0 +1,5 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzJCVEY0SzRjcThLUVBR";
+	var decoded01 = atob(str01);
+	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
---
+++ files/ps3/js/Call_of_Duty_World_at_War.js
@@ -0,0 +1,12 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzdwMVVJR0ZBTkZWd21BL0NhbGwlMjBvZiUyMER1dHklMjBXb3JsZCUyMGF0JTIwV2FyJTIwJTVCMDElNUQucGtn";
+	var decoded01 = atob(str01);
+	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzdwMVVJR0ZBTkZWd21BL0NhbGwlMjBvZiUyMER1dHklMjBXb3JsZCUyMGF0JTIwV2FyJTIwJTVCMDIlNUQucGtn";
+	var decoded02 = atob(str02);
+	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzdwMVVJR0ZBTkZWd21BL0NhbGwlMjBvZiUyMER1dHklMjBXb3JsZCUyMGF0JTIwV2FyJTIwJTVCMDMlNUQucGtn";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
