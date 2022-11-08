5eabfb9d612e275988a3a916b4230f881f56da02
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-09 01:35:20 +0300

    Add files via upload

---
+++ files/ps3/js/Condemned_2_Bloodshot.js
@@ -0,0 +1,12 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3Fhc1Rxb0QtbUc5bmV3";
+	var decoded01 = atob(str01);
+	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1VmSk81Szdvd1E5cTBB";
+	var decoded02 = atob(str02);
+	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1dYU0thQmZsNno0SkdB";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
