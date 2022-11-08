6928824ce9e6ca26275c40b05b873cb8e9b54e19
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-09 01:42:55 +0300

    Add files via upload

--- files/ps3/js/Crysis_2.js
+++ files/ps3/js/Crysis_2.js
@@ -1,11 +1,11 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3ZOMGhFSjdlVXJ6QU53";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0xhWkdyTzluRUx4SWpR";
 	var decoded02 = atob(str02);
-	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL292LVdLRk9VREtjZUlR";
 	var decoded03 = atob(str03);
