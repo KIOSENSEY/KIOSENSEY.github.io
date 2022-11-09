47af840c3ae6aaba5f5820ff7f8532d01315aa4b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-09 09:57:45 +0300

    Add files via upload

---
+++ files/ps3/js/Dantes_Inferno.js
@@ -0,0 +1,12 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL001UEd3WXpGUU1Id1dBL0RhbnRlJ3MlMjBJbmZlcm5vLjEucGtn";
+	var decoded01 = atob(str01);
+	var link01 = "1QWER" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL001UEd3WXpGUU1Id1dBL0RhbnRlJ3MlMjBJbmZlcm5vLjIucGtn";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL001UEd3WXpGUU1Id1dBL0RhbnRlJ3MlMjBJbmZlcm5vLjMucGtn";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
