ebf4fcded6d3f1969fb7ae0a3fbd88049d39357d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-09 11:41:54 +0300

    Add files via upload

--- files/ps3/js/Call_of_Duty_Ghosts.js
+++ files/ps3/js/Call_of_Duty_Ghosts.js
@@ -1,11 +1,11 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0xGeFFyS0txQTlfb25B";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2RXbzhFeUd1N2JzSjBB";
 	var decoded02 = atob(str02);
-	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0hXUzBrcnBJX0NHQWJB";
 	var decoded03 = atob(str03);
@@ -17,4 +17,4 @@
 	
 		var str05 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1AxMUE0MmEyTjNiQ0FR";
 	var decoded05 = atob(str05);
-	var link05 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);	
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);	
--- files/ps3/js/Call_of_Duty_Moder_Warfare_3.js
+++ files/ps3/js/Call_of_Duty_Moder_Warfare_3.js
@@ -1,11 +1,11 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzVKamJCLWlEb3h0OGtB";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0FQdV9uM3ZxZ0piN1Zn";
 	var decoded02 = atob(str02);
-	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzYwTEd2SzBYMTlwSnd3";
 	var decoded03 = atob(str03);
