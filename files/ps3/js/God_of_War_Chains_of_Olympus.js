349722fb38ce08197ff3e59a870e0a2e54496839
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-13 17:05:01 +0300

    Add files via upload

---
+++ files/ps3/js/God_of_War_Chains_of_Olympus.js
@@ -0,0 +1,8 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2VBMHBMQk9PQ1ljSy1R";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2o3bzVZWHBGUjRUZFJn";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
