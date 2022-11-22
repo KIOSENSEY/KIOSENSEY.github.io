823ea708f901f29d83850a4667e560c68d76584c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-22 12:26:25 +0300

    Add files via upload

--- files/ps2/js/Silent_Hill_3.js
+++ files/ps2/js/Silent_Hill_3.js
@@ -1,3 +1,3 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy83UTR2ZmZBdXdTa3pXM3p4aHlRNmtQYlNiOWF1";
+		var str01 = "aHR0cHM6Ly9zaGFyZWRieS5ibG9tcC5jb20vVWJZTE1u";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
