932914422dc7dcb7657fa91d3d22ea7ad002deef
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-25 21:41:48 +0300

    Add files via upload

---
+++ files/ps3/js/ULTIMATE_MARVEL_VS_CAPCOM_3.js
@@ -0,0 +1,8 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0Mwd3pxdnE1N200MGdn";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2VPT3dIMGNaTWtlbEV3";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);	
