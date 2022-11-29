b00ccc092690fae784d67689c570645160ef562c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-11-29 09:44:54 +0300

    Add files via upload

--- files/ps2/js/Silent_Hill_2.js
+++ files/ps2/js/Silent_Hill_2.js
@@ -1,3 +1,3 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9nUFp3N1JEYjhiOTROdE5mMzNUYWIyd3lCQXdq";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9pTnd1Q1JQaWl5WDhHVkY4OHdna3hRNWlWQUdm";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
