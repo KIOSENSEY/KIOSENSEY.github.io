3795321c0c7c4677c89b6acc30eb04340a2a6ed0
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-02 12:43:13 +0300

    Add files via upload

---
+++ files/ps4/js/The_Dark_Pictures_Anthology_House_of_Ashes.js
@@ -0,0 +1,7 @@
+		var str01 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbXpFa19mTGFwUmtnbnc0RHZEX0FRRnJpMnlI";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbXpFa19mTGFwUmtnbnNDbjlHUzR0dmRNbE9W";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
