e9d4a33ce6b17c48165909dc3d9584f8f230e3d5
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-07 17:00:27 +0300

    Add files via upload

---
+++ files/ps3/js/Derrick_The_Deathfin.js
@@ -0,0 +1,23 @@
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL29MTlFqVVFucU5JWlBR";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
