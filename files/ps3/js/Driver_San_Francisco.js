c315833d738f62ce37c367c4d74c833cedeae3fe
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-08 13:21:42 +0300

    Add files via upload

--- files/ps3/js/Driver_San_Francisco.js
+++ files/ps3/js/Driver_San_Francisco.js
@@ -10,4 +10,8 @@
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1VkcFdzNTRIajBTT1h3L0QuUy5GJTIwJTVCMDMlNUQucGtnP3c9MQ==";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5jb20vZC9PRXNxcFp6cEdPN2tiQQ==";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);	
 		
