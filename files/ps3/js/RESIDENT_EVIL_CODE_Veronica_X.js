cd722409107204c387d0f6e28f338ca28fd97096
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-11 22:19:40 +0300

    Add files via upload

--- files/ps3/js/RESIDENT_EVIL_CODE_Veronica_X.js
+++ files/ps3/js/RESIDENT_EVIL_CODE_Veronica_X.js
@@ -7,7 +7,7 @@
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0V1X2hHMlJHRXE1dmRR";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
