bc7692919cabdf44bdfde3c96c1aad59daf3db96
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-13 18:38:17 +0300

    Add files via upload

---
+++ files/ps3/js/Uncharted_Drake_s_Fortune.js
@@ -0,0 +1,24 @@
+
+		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvNmFFMkVzTE5IMzl4X3c=";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly95YWRpLnNrL2Qvb25ldVM2cHQzSi1XVGc=";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvOXlsZURGRVhOZHpIOVE=";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly95YWRpLnNrL2QvR2xBLXlxNkRPUWtwbVE=";
+	var decoded04 = atob(str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "aHR0cHM6Ly95YWRpLnNrL2Qvc0RDNjhrcGwzQ2hfdHc=";
+	var decoded05 = atob(str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "aHR0cHM6Ly95YWRpLnNrL2QvR19NN0ZVVDB5a2dySGc=";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
