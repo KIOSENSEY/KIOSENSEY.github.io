4824b3440667680132ada55f0766afa75fb23b53
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-13 15:47:12 +0300

    Add files via upload

--- files/ps3/js/Army_of_Two_The_40th_Day.js
+++ files/ps3/js/Army_of_Two_The_40th_Day.js
@@ -1,12 +1,12 @@
-		var str01 = "";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8zUzExODZmWGo2U2ZpaERXUk5ZTkJqVjY2R050";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9aQlBEdHlRdTVDakF6VjhDenc0UGk0ellBeUY5";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9nWE5EU2loNzJaeFdEOVR2eGZrQVNGMzE1UGJ2";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
