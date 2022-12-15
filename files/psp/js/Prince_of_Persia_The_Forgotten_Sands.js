4d4c57146b2bacd9ddd7f731f97e17b9a1fec93d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-15 16:16:22 +0300

    Add files via upload

---
+++ files/psp/js/Prince_of_Persia_The_Forgotten_Sands.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0s3bmF4d213aVA5T2pB";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
