ae7562435cacd2eb0232ba5580123e75cca50e02
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-17 12:21:13 +0300

    Add files via upload

---
+++ files/ps4/js/Spintires_Mudrunner.js
@@ -0,0 +1,23 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTYwNjM0NDg/aGFzaD03TVgwMWRxQXVYRmJEeEE5RG9VZnphTmFaWWpLUEcwU0ppanJhc0VwanRYJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTYwNjM1ODM/aGFzaD1sQnpaWU9mem1TWUV1VnNvS2FSNlJ6VmRadDh6dndJemVSeDhudnhiUDhQJmRs";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTYwNjM1OTU/aGFzaD04emx2RXpmRDFtVWZyRWR5aEVGNTRZaGhCblo4WlpjYm9IcU9Ld1JFeXI0JmRs";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
