6811a653afe5621a67363ba84bce908fdbab393a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-17 18:40:43 +0300

    Add files via upload

---
+++ files/ps2/js/Resident_Evil_Outbreak.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9XWDdWU0c1eUF5dlZ3M1E2RHc4QXQydHd0d0ZS";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
