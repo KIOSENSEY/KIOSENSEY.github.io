c8934fc800a0103d520aa06282977dd826431ffe
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-18 17:14:49 +0300

    Add files via upload

---
+++ files/ps3/js/Prince_of_Persia.js
@@ -0,0 +1,24 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzd3ZHVzZURVc1dhMkVB";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "https://disk.yandex.ru/d/kJy7xHCt2nSDRQ";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1otd05UT2ZuNjR1aVR3";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
