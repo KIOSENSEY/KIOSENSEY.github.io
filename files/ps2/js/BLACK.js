95f81724e14e0158482adf164fbf3b5617eaf875
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-18 11:25:11 +0300

    Add files via upload

---
+++ files/ps2/js/BLACK.js
@@ -0,0 +1,4 @@
+
+		var str01 = "https://disk.yandex.ru/d/Ex2nFbhQAbzVUg";
+	var decoded01 = atob(str01);
+	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
