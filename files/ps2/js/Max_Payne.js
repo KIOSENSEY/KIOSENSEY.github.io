ff685ab1ed26c3f9d5387b31fa690f7af805b2df
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-20 12:04:36 +0300

    Add files via upload

---
+++ files/ps2/js/Max_Payne.js
@@ -0,0 +1,4 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0lKQlJLaWYxZjFObjd3";
+	var decoded01 = atob(str01);
+	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
