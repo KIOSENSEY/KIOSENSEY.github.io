7820b1da571af81a37164399da6f3be38a0c9b1e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 15:28:45 +0300

    Add files via upload

---
+++ files/ps1/js/FIGHTING_FORCE.js
@@ -0,0 +1,4 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL05ZY21xYWZlb0hNODdB";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
---
+++ files/ps1/js/Gran_Turismo_2.js
@@ -0,0 +1,8 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzhUdVJuYUdJVEtPLThR";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0lnbEJkYVpuMXVySWhB";
+	var decoded02 = atob(str02);
+	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded02);	
