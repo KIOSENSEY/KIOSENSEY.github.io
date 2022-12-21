d90b490f8b8526d1315c93686e7732899d410cfc
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 16:10:54 +0300

    Add files via upload

---
+++ files/ps1/js/Jojos_Bizarre_Adventure.js
@@ -0,0 +1,4 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2RZZm1lc0lYNWdRWGpn";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
