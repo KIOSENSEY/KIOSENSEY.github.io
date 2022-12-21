940c9185b2361ba677a793fce7cfdfa0fac236be
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 19:03:48 +0300

    Add files via upload

--- files/ps2/js/Mortal_Kombat_Deception.js
+++ files/ps2/js/Mortal_Kombat_Deception.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3YyanRZOU5KdFU4eGlB";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2FNeXZMX3psMy01WElB";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
