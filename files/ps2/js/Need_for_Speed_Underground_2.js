ac4ab4b776483048330fb32bca5d913306a9e241
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 15:55:06 +0300

    Add files via upload

--- files/ps2/js/Need_for_Speed_Underground_2.js
+++ files/ps2/js/Need_for_Speed_Underground_2.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL21TVUVvQ3gtZW54ak1n";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
--- files/ps2/js/Silent_Hill_3.js
+++ files/ps2/js/Silent_Hill_3.js
@@ -1,3 +1,3 @@
 		var str01 = "aHR0cHM6Ly9zaGFyZWRieS5ibG9tcC5jb20vVWJZTE1u";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
