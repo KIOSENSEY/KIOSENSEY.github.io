fb11ff1a951a979de949e13472f29f1078a867bf
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 21:02:08 +0300

    Add files via upload

--- files/ps2/js/Silent_Hill_4_The_Room.js
+++ files/ps2/js/Silent_Hill_4_The_Room.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy91alh0MThUTlk4dkFUOFNZeVFoVDYzYnVQODl6";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
