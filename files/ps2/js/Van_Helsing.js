f879017cbb97b8d8ccac412cee3ea5910d34ca89
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 20:39:28 +0300

    Add files via upload

--- files/ps2/js/Van_Helsing.js
+++ files/ps2/js/Van_Helsing.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yWlg4VjRqalJ0dWE0UlJ0a2lOZkZmUHl0U3c0";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy83Zmhoazc4ekI0QUR6V05oRFd5Rkd3dVRhVlYx";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps2/js/Yakuza.js
+++ files/ps2/js/Yakuza.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yWlg4VjRqalJ0dWE0UlJ0a2lOZkZmUHl0U3c0";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzFjSV8wYW85UGFVakxR";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
