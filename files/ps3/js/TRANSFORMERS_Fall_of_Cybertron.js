ba15139268aba14592496bc41be6928c62833766
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 22:29:45 +0300

    Add files via upload

--- files/ps3/js/TRANSFORMERS_Fall_of_Cybertron.js
+++ files/ps3/js/TRANSFORMERS_Fall_of_Cybertron.js
@@ -1,20 +1,20 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1NtTTYwTk5SSzJOR2d3L1RSQU5TRk9STUVSUyUyMEZhbGwlMjBvZiUyMEN5YmVydHJvbi4lNUIwMSU1RC5wa2c=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1NtTTYwTk5SSzJOR2d3L1RSQU5TRk9STUVSUyUyMEZhbGwlMjBvZiUyMEN5YmVydHJvbi4lNUIwMiU1RC5wa2c=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1NtTTYwTk5SSzJOR2d3L1RSQU5TRk9STUVSUyUyMEZhbGwlMjBvZiUyMEN5YmVydHJvbi4lNUIwMyU1RC5wa2c=";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1NtTTYwTk5SSzJOR2d3L1RSQU5TRk9STUVSUyUyMEZhbGwlMjBvZiUyMEN5YmVydHJvbi4lNUIwNCU1RC5wa2c=";
 	var decoded04 = atob(str04);
-	var link04 = (decoded04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3ozb0g1MzhuYkV6LWpn";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + (decoded05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
