b267029d80ac51833db1ae74e20d3c74a5f0a777
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 21:09:01 +0300

    Add files via upload

--- files/ps2/js/BLACK.js
+++ files/ps2/js/BLACK.js
@@ -1,4 +1,4 @@
 
-		var str01 = "https://disk.yandex.ru/d/Ex2nFbhQAbzVUg";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0V4Mm5GYmhRQWJ6VlVn";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps2/js/Grand_Theft_Auto_Liberty_City_Stories.js
+++ files/ps2/js/Grand_Theft_Auto_Liberty_City_Stories.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2NVOXlVNzVaVk03OF9B";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
