f0364f331a337ebd8053bcd0749e2d08c3e4d8e4
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 21:35:34 +0300

    Add files via upload

--- files/ps3/js/Beyond_Two_Souls.js
+++ files/ps3/js/Beyond_Two_Souls.js
@@ -1,10 +1,10 @@
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2JSbC02eHEtaHJTMUxB";
 	var decoded01 = atob(str01);
-	var link01 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0RYa0c1WnhXVS1xdUV3";
 	var decoded02 = atob(str02);
-	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzY1VVVHbHJmbkJNUmhB";
 	var decoded03 = atob(str03);
@@ -16,7 +16,7 @@
 	
 		var str05 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3c5ZG00akNxRGtJVnFR";
 	var decoded05 = atob(str05);
-	var link05 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
 		var str06 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3Vhb1JSanFTRkJwMlJ3";
 	var decoded06 = atob(str06);
@@ -32,7 +32,7 @@
 	
 		var str09 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL29pX2VRNXdJM1RGTEVR";
 	var decoded09 = atob(str09);
-	var link09 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str09);
+	var link09 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str09);
 	
 		var str010 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2lDSm51dFNENGpTVlZB";
 	var decoded010 = atob(str010);
