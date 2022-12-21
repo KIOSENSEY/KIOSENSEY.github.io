abe7d91f4fdfa576d78d608d534eabfcede1285e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 19:25:35 +0300

    Add files via upload

--- files/ps2/js/Predator_Concrete_Jungle.js
+++ files/ps2/js/Predator_Concrete_Jungle.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9aek5oMjVaM2JHUFNBNDZYdk51RlZoelR6aHg5";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9QaGphdEd1RFg3d2F4Q1c1OTZhd0ZSVmcyNEFY";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
--- files/ps2/js/Red_Dead_Revolver.js
+++ files/ps2/js/Red_Dead_Revolver.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9aek5oMjVaM2JHUFNBNDZYdk51RlZoelR6aHg5";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0REcjVKR1NTLTk1eFZB";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
