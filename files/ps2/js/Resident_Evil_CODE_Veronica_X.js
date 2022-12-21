07fea05bc9966dcc3b43162c9e7904ba346c546f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-21 19:39:10 +0300

    Add files via upload

--- files/ps2/js/Resident_Evil_CODE_Veronica_X.js
+++ files/ps2/js/Resident_Evil_CODE_Veronica_X.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yaDFQNGF6elhZTlNEQ2gzR1RTMjVXaTN3Q0Ri";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3pwRWZ5YS1WWktNNzZB";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
--- files/ps2/js/Rogue_Trooper.js
+++ files/ps2/js/Rogue_Trooper.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yaDFQNGF6elhZTlNEQ2gzR1RTMjVXaTN3Q0Ri";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9TN3dndlBXQ3diZmgzUUdYWFoxQjJGQTZ6RjdZ";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
