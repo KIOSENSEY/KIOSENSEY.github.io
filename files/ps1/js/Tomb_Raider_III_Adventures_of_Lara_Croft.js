7d44cd2c3211a7bd4360e0b9d9e648e9f1348246
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-22 10:10:07 +0300

    Add files via upload

---
+++ files/ps1/js/Tomb_Raider_III_Adventures_of_Lara_Croft.js
@@ -0,0 +1,4 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzhFa2w4RDBTNmFvOHh3";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
