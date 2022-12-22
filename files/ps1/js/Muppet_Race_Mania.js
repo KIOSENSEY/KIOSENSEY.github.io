2f96a5ddf4d0ea5e4af57e8b2652dd41d7e646f6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-22 09:05:09 +0300

    Add files via upload

---
+++ files/ps1/js/Muppet_Race_Mania.js
@@ -0,0 +1,4 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0l4ME9BRHFkV0dwOWJR";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
