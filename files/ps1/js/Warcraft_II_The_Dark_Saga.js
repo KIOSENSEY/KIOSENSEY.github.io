2f11253d7939feb5cc91f924de3e5bb101f9237a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-22 10:25:50 +0300

    Add files via upload

--- files/ps1/js/Warcraft_II_The_Dark_Saga.js
+++ files/ps1/js/Warcraft_II_The_Dark_Saga.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzY2S3h6dDRjbl9LUFhR";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2s0Q09PZk1haWN2WG5n";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
