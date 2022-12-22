a82833d10a94037eba0135d096e3eaee503edc4f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-22 14:14:19 +0300

    Add files via upload

---
+++ files/ps3/js/Killzone_3.js
@@ -0,0 +1,24 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2lJY3hnOGh6eVprYU5R";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3JyeFlGWWVfbXZ3NWZR";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0phRVc0VjhIcXAxMFhn";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2dFTWJWWDR1ajd6aDR3";
+	var decoded04 = atob(str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3VKc1dhenExR3d1dTB3";
+	var decoded05 = atob(str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL01RU0hnelI3TFh6OFpR";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
