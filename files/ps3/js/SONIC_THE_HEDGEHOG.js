90b817b42810065aba6aeae317e863af49de5165
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-23 10:46:37 +0300

    Add files via upload

--- files/ps3/js/SONIC_THE_HEDGEHOG.js
+++ files/ps3/js/SONIC_THE_HEDGEHOG.js
@@ -1,17 +1,17 @@
 
-		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvSGpDNDc2cGo1Z3FqY0E=";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy94V0ZXeVROWlBYWDNZdHo4eUJneWt0VHZaUVNO";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvWGlJX3ljNVFNSlJhRmc=";
+		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy82ZkJOM2Z4WDVCQmdrdmhCakFEa3Q0NXl2dmZQ";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvYmlxblkxa3RyNjduSFE=";
+		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9hWDk5eGlrM1NqZ1J6NzNSNUZ0OXViR0F1TjNO";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly95YWRpLnNrL2QvTVNJU3V0YkV5TTBQUUE=";
+		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9iVHg5VDUzU0MxdDRoU1RBaXh1RlNrVjQzeEdG";
 	var decoded04 = atob(str04);
 	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
