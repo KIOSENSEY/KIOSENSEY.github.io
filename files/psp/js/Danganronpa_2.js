d097d54dd7788ef5ccbb72cdb53de133e65c24a4
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-27 22:54:24 +0300

    Add files via upload

---
+++ files/psp/js/Danganronpa_2.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY1MjAzMTE/aGFzaD1WRmcwNXhqdkdHeGtNQlJub0Q2QWpaOHFmdktOc1dHWm1GVFcwd1pNT2tUJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
---
+++ files/psp/js/God_of_War_Ghost_of_Sparta.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY4MDI5NzA/aGFzaD13b3Izc0N4NFJrMW1xemlMc0hQZlh4UXBtY2gyRER6b1BOT0ZQY0ViTHo0JmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
