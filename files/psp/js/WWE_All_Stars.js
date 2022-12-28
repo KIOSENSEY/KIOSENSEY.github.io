01050144a65568a76e1ecb2965e452f82e6b814d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-28 12:55:25 +0300

    Add files via upload

---
+++ files/psp/js/WWE_All_Stars.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY4MjM4Mjg/aGFzaD1HdWJ1amxwbndkU1NQNVEzUmNOcHNsVnBsVVh0a1V3Q3doSTFkcGFTV1VUJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
