85302e8f06193a95d27b042f65ff3520f39b6ead
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-28 13:57:06 +0300

    Add files via upload

---
+++ files/psp/js/FlatOut_Head_On.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY4MjQxMjU/aGFzaD1xTHNyVTdDZ25WbmRvdG9kSWJjSUl2VjVCa0JDbXgzUkpNRDhFUDhDYjlUJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
