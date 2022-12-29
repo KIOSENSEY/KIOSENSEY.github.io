44b324fcad6672f48dfe73ba65ed4fa9f75b620a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-29 17:11:43 +0300

    Add files via upload

--- files/ps3/js/Resident_Evil_4.js
+++ files/ps3/js/Resident_Evil_4.js
@@ -3,13 +3,13 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY5MTc3MjI/aGFzaD1GTEdmZkdYWVA2MzdqVWdLdHc4ZUpPd1JXMmNTaVhyeFZJSnNab2RKeTJzJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
