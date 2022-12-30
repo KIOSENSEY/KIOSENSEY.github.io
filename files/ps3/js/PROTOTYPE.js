2897bb76f0a878549a692327da2ed77a6e09da8b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-30 11:16:13 +0300

    Add files via upload

---
+++ files/ps3/js/PROTOTYPE.js
@@ -0,0 +1,24 @@
+
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1RHZ2otRUdWR29iOEV3";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1ZLMHJqSDB5MkxWTzZ3";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0h4aGljYjVBWVVyRmJn";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
---
+++ files/psp/js/RESISTANCE_Retribution.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY5NTE2Mjg/aGFzaD1hc1pFSTNpRUJPcW1seXRPWXgxczMxcUlsblpaZ0YxaTl0Wm9TZ2k1dUtMJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
