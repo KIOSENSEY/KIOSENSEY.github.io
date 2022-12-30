17e485a9ee9c465d1ee572fbba81aae1c9b01884
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2022-12-30 11:31:39 +0300

    Add files via upload

--- files/ps4/js/SnowRunner.js
+++ files/ps4/js/SnowRunner.js
@@ -1,8 +1,8 @@
-		var str01 = "";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9Ea2lYWUZHM0R4NDh2N0ZGQjVXd3RUQVo0WjhD";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9CWWpHQll1NDVQZmFDN2FaWXQzazE1Rnk2MUFS";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
