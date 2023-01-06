49efa051a836d76f582412d4be2dce8a7abf837d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-06 20:07:37 +0300

    Add files via upload

---
+++ files/pspps1/js/Breath_of_Fire_III.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc0MTU5NzA/aGFzaD1mMGhUNXZNVU1ubHZYSXJlUG5EV1FNaUdTV0xCWWdPclpRbFptUG5abFp3JmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
