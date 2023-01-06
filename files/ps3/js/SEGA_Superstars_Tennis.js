6af94f5edb5447b805823e235c8254d9ab92daa6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-06 10:42:45 +0300

    Add files via upload

--- files/ps3/js/SEGA_Superstars_Tennis.js
+++ files/ps3/js/SEGA_Superstars_Tennis.js
@@ -1,8 +1,9 @@
-		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTczODI4MTI/aGFzaD14NzdoMGdRSnZSN0tPSGx1MDZDZTNvMGRjdTMxQWliZWdhd1diZVFacXJvJmRs";
+
+		var str01 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBcGVuM2NiekQ1MmthN2pUeXlIclFFdnMwZU0=";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTcyNzc1NjU/aGFzaD1aNVAyOVd2TnRaZVpmbDBLQ3BhbDhac3pKZDF2OXdpN1E4OHpjSEMxMDEwJmRs";
+		var str02 = "";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
@@ -12,12 +13,12 @@
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
