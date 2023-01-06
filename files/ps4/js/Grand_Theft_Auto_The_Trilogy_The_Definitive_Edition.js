c0ba795a38fdac7e7e1d1529cc990e35da103b1d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-06 15:02:27 +0300

    Add files via upload

--- files/ps4/js/Grand_Theft_Auto_The_Trilogy_The_Definitive_Edition.js
+++ files/ps4/js/Grand_Theft_Auto_The_Trilogy_The_Definitive_Edition.js
@@ -1,8 +1,8 @@
-		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTczOTY1Nzk/aGFzaD1MMm9qekFpY3NJMEZ4S3FoOXlmYW9MeklrNzBFSmdrVFNYUXN6cURaSW04JmRs";
+		var str01 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBczlRcnZUakdISnZnWHZCOGMzMEx4VHNfVkN1";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8xRjZrYnR1VlEyWDdpOURrWDRhUTdZa3hRN3Vo";
+		var str02 = "";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
