72b09eec0ec772598356da2800c2d225bb18c32f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-09 13:13:19 +0300

    Add files via upload

--- files/ps3/js/Rock_of_the_Dead.js
+++ files/ps3/js/Rock_of_the_Dead.js
@@ -1,4 +1,4 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8zTjRiRzRnallDWHR0Mlh1WVBHQjNGR3U5WFZ4";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1Njk4NjQ/aGFzaD14ckN6TUJscHZZcHo5Z3ZHenlKUGxUaTBvd1VPU2FhRmZ2TXdBbWJsZm9zJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
