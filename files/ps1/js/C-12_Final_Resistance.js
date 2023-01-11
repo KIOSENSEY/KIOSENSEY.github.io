53803c207d8d4ec8aa2e20bfd27b9b67d240859a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 15:58:30 +0300

    Add files via upload

---
+++ files/ps1/js/C-12_Final_Resistance.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc3MTM0NjA/aGFzaD15N3lXNTdvaTUzZ3ZRSU01QmVhTHo5UVZLYnFaWnlUWEx2Sk1HVUtRZTRMJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
