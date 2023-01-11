9a48c3481c33d5d3325b2b9ccdfe754e7f2c3816
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 20:19:28 +0300

    Add files via upload

--- files/ps3/js/Battlefield_4.js
+++ files/ps3/js/Battlefield_4.js
@@ -1,17 +1,17 @@
 
-		var str01 = "";
+		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvZ21LQUczakMtZWw1cHc=";
 	var decoded01 = atob(str01);
 	var link01 = "http://q32.ru/65495/" + (decoded01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvOTVGUDJYOWVSWkg5YVE=";
 	var decoded02 = atob(str02);
 	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvWkV0aHlTM21pYkxEcUE=";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded03);
 	
-		var str04 = "";
+		var str04 = "aHR0cHM6Ly95YWRpLnNrL2QvMzZBTWRHQjNDb2ZpOVE=";
 	var decoded04 = atob(str04);
 	var link04 = (decoded04);
 	
