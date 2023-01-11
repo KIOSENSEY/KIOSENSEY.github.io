d9a3af6f53226b031d37966b8d35b9174cdc4373
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 09:22:23 +0300

    Add files via upload

--- files/ps3/js/Child_of_Light.js
+++ files/ps3/js/Child_of_Light.js
@@ -1,12 +1,12 @@
 
 		var str01 = "aHR0cDovL2FyZXMuZGwucGxheXN0YXRpb24ubmV0L2Nkbi9FUDAwMDEvTlBFQjAxOTM0XzAwL3Bma1FjelV4RlN5WEV2ZmdzYUJyTUVuV0Z2eWlGRUVvQ3VVVFRSeXJCcExmdGhYcU9hSU1YVHdTUm9aUlR1dmwucGtn";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cDovL2IwLnd3Lm5wLmRsLnBsYXlzdGF0aW9uLm5ldC90cHBrZy9ucC9OUEVCMDE5MzQvTlBFQjAxOTM0X1QyL2U1MGNkYjBmZDgzNWUwNDcvRVAwMDAxLU5QRUIwMTkzNF8wMC1DSElMRE9GTElHSFQwMDAxLUEwMTAyLVYwMTAwLVBFLnBrZw==";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0tNdkU5U0U1S2xQSGVR";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
+	var link03 = (decoded03);	
--- files/ps3/js/Damage_Inc._Pacific_Squadron_WWII.js
+++ files/ps3/js/Damage_Inc._Pacific_Squadron_WWII.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb0ZHeVZlWWZDX1VnMEUxVHlVdGhrVjFqbmd3";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);	
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);	
