504bcdf157d152b55c63f9d091481eafdafb9f6b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 14:43:40 +0300

    Add files via upload

--- files/ps3/js/Natsuiro_High_School_Seishun.js
+++ files/ps3/js/Natsuiro_High_School_Seishun.js
@@ -1,23 +1,23 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc3MDU1ODM/aGFzaD1GOGFUM3pVQVNEMXJOajhwMm5JeDJqMDBiY0pWUEM3eEtiYkFaOFBqN2xrJmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
 	
 		var str03 = "";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = (decoded05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+	var link06 = (decoded06);
