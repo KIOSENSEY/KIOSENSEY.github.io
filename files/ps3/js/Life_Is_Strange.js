72e219d05734a80d871a3e795b9bdc82ff837519
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 09:40:01 +0300

    Add files via upload

--- files/ps3/js/Life_Is_Strange.js
+++ files/ps3/js/Life_Is_Strange.js
@@ -1,24 +1,24 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1gwTmNtWTNUVHd2Nzh3";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2ZYN1o4Vy16eFJoaTBR";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2xSemdISDJvUjlUR2FR";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
 	
-		var str04 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMTQzMzYwND90b2tlbj1sMUhLaU1IeXJYM2JlTzUmZmlsZW5hbWU9TGlmZSBpcyBTdHJhbmdlIFtOUEVCMDIxOTddIFJ1U291bmQgRXAuMS5wa2c=";
+		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://q32.ru/65495/" + (decoded05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
+	var link06 = (decoded06);	
--- files/ps3/js/Lollipop_Chainsaw.js
+++ files/ps3/js/Lollipop_Chainsaw.js
@@ -1,8 +1,8 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0VfZm9WWk03b29Nc1R3L0wuQy4lMjAlNUIwMSU1RC5wa2c=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0VfZm9WWk03b29Nc1R3L0wuQy4lMjAlNUIwMiU1RC5wa2c=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
