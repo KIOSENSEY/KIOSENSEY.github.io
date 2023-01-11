1067b384353a3901d9bde9b53de73584f856ba9b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-11 14:58:43 +0300

    Add files via upload

---
+++ files/ps2/js/UFC_Throwdown.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc3MDg5ODA/aGFzaD05d2ZmamU2d2M5WnhYUVJZSkZMOFpIbjlqQjNxT2Nac29LSTV5WlR4eldvJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
