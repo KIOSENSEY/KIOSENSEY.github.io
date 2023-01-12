066f31836a9791d37321dfce1fb221f68c40930a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 17:36:55 +0300

    Add files via upload

---
+++ files/ps1/js/Sled_Storm.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc3NzI0NjA/aGFzaD1JY0JORWlKSWJ4dmZVVEVNRXpianBXNWE4VFlMazNianpHbmNXNkVucXFIJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
