011ea17e917bdde52361dfaea9501ba6221d4a0b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 19:08:21 +0300

    Add files via upload

--- files/ps3/js/Devil_May_Cry_HD_Collection.js
+++ files/ps3/js/Devil_May_Cry_HD_Collection.js
@@ -1,16 +1,16 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1FmYlk3dzJ3NWs4ZWdR";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3ktUnIzNGhvMkFQNUV3";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2pHLXA4N3hSeTdnc0ZR";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1hYN1lOMUJHVTN1enFR";
 	var decoded04 = atob(str04);
-	var link04 = (decoded04);	
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);	
