7ddfa1a78ff6e5ff94903092d029fd9cd1700cbe
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 20:00:30 +0300

    Add files via upload

--- files/ps3/js/Sports_Champions_2.js
+++ files/ps3/js/Sports_Champions_2.js
@@ -1,8 +1,8 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2ZwcU12dS1vV1g0Uzln";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2kwaDZzcUVySnhBLUdB";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
