5f487df38d07f85529eb4bf5081a75c3be5ee78d
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 20:30:21 +0300

    Add files via upload

--- files/ps2/js/Pirates_of_the_Caribbean_The_Legend_of_Jack_Sparrow.js
+++ files/ps2/js/Pirates_of_the_Caribbean_The_Legend_of_Jack_Sparrow.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yaDFQNGF6elhZTlNEQ2gzR1RTMjVXaTN3Q0Ri";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
