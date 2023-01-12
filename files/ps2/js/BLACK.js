a5fb589512a371e651ceefded68e70de5d1444c1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 20:19:37 +0300

    Add files via upload

--- files/ps2/js/BLACK.js
+++ files/ps2/js/BLACK.js
@@ -1,4 +1,4 @@
 
 		var str01 = "https://disk.yandex.ru/d/Ex2nFbhQAbzVUg";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps2/js/K-1_World_GP_2006.js
+++ files/ps2/js/K-1_World_GP_2006.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9CNHdBV1lRQVc4MUY1RjZnVnU5NlRWNFEyYlNH";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
