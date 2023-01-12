e359815229be2547906c8f202d29bb9ea5604136
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 18:50:21 +0300

    Add files via upload

--- files/ps3/js/BEN_10_OMNIVERSE.js
+++ files/ps3/js/BEN_10_OMNIVERSE.js
@@ -1,6 +1,6 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc2MzU3MjU/aGFzaD1TOGV5VHdJR0JLZXp4YTlrY1o3TDdrVFBnUTV1UGJ0VFEzU0ppVVA1MmdzJmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "";
 	var decoded02 = atob(str02);
