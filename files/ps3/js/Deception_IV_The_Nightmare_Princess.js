f54d6568412d20631e5246634fb86931da0132e6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-12 17:11:23 +0300

    Add files via upload

---
+++ files/ps3/js/Deception_IV_The_Nightmare_Princess.js
@@ -0,0 +1,23 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc3NzQ2MzA/aGFzaD1VYVN0QnBESmFCSVR1TlE0MnBjMjZkbk1pNHlZR3hEb05aTGdUa3dLN2VnJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	
+		var str02 = "";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
+	
+		var str03 = "";
+	var decoded03 = atob(str03);
+	var link03 = (decoded03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = (decoded05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = (decoded06);
