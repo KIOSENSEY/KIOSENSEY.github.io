79dfe2266d1b9c28f3149ff281e5481a176c2b45
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 12:52:47 +0300

    Add files via upload

--- files/ps3/js/WWE_2K16.js
+++ files/ps3/js/WWE_2K16.js
@@ -1,15 +1,15 @@
 		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9XdmdpdVpmOUdoQ0Y5NmZ1dmZRUHp1UVRoMzJS";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9RNEdGMnQ3TjZYVzFSOXdUOWp5dTg0U2k1QlR1";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9qYmZaWGZRdFZ6QlhUaGsxWkd5dmtZZlQxYno5";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
 	
 		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9YeXRqMnpOamtYR1o0Q1BDWTM4eDNHYjR5NEI2";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
