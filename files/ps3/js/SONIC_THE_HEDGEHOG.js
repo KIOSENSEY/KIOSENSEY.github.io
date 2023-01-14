c82a271a0cddc8a1f71d5c89ae47620f8596eea6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 16:00:06 +0300

    Add files via upload

--- files/ps3/js/SONIC_THE_HEDGEHOG.js
+++ files/ps3/js/SONIC_THE_HEDGEHOG.js
@@ -1,24 +1,25 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy94V0ZXeVROWlBYWDNZdHo4eUJneWt0VHZaUVNO";
+		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9iVHg5VDUzU0MxdDRoU1RBaXh1RlNrVjQzeEdG";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy82ZkJOM2Z4WDVCQmdrdmhCakFEa3Q0NXl2dmZQ";
+		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9hWDk5eGlrM1NqZ1J6NzNSNUZ0OXViR0F1TjNO";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9hWDk5eGlrM1NqZ1J6NzNSNUZ0OXViR0F1TjNO";
+		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy94V0ZXeVROWlBYWDNZdHo4eUJneWt0VHZaUVNO";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9iVHg5VDUzU0MxdDRoU1RBaXh1RlNrVjQzeEdG";
+		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy82ZkJOM2Z4WDVCQmdrdmhCakFEa3Q0NXl2dmZQ";
 	var decoded04 = atob(str04);
-	var link04 = (decoded04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + (decoded05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = (decoded06);	
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
+	
--- files/ps3/js/Sonic_SEGA_All_Stars_Racing.js
+++ files/ps3/js/Sonic_SEGA_All_Stars_Racing.js
@@ -1,11 +1,11 @@
 		var str01 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xZTk4ZHJjM3Z0dEZMZ28yQXY3REU4NG03dmQ1R3NHZk0vdmlldw==";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xOW81SVJ4QXEwVWowc2JhRFd5UC0yRnZ3cE5iT053V1Avdmlldw==";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xSHhHcW0teGUwaGNCQ0lCMTFLQmx6NkJOOFBPaGxFMXQvdmlldw==";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
