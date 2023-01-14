6bbdb29612417fee3e350e97bd787c6346eacc03
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 11:31:44 +0300

    Add files via upload

--- files/ps3/js/Counter_Strike_Global_Offensive.js
+++ files/ps3/js/Counter_Strike_Global_Offensive.js
@@ -1,12 +1,12 @@
 
-		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTcxNjYzNDk/aGFzaD1SbkxVUEtCTGY2N1BsWkNlWUt4VFdpM213YXg4NXpERkxmalBma1dZREk0JmRs";
+		var str01 = "aHR0cDovL3pldXMuZGwucGxheXN0YXRpb24ubmV0L2Nkbi9VUDIwNjEvTlBVQjMwNTg5XzAwL3BURkVyaWxURUdHRXRweWpqU0lvbXVaVW1vTENRY0Voa2NBUFpobnJiTklJZHpMQUlzeHV3dW1WUFVpbFhBSE9iaEdzUEJPVEhsa3NFYnRnZnZKaXFJT1FJQ2x3Z1B2cFJ0Zk1pLnBrZw==";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cDovL2IwLnd3Lm5wLmRsLnBsYXlzdGF0aW9uLm5ldC90cHBrZy9ucC9OUFVCMzA1ODkvTlBVQjMwNTg5X1QwLzEwMjVlYTY1NzdhYTk1ZWUvVVAyMDYxLU5QVUIzMDU4OV8wMC1IRERCT09UQ1NHTzAwMDAxLUEwMTAxLVYwMTAwLVBFLnBrZw==";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "http://q32.ru/65495/" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0ZHUWxNZ0g4Sl81clp3";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
