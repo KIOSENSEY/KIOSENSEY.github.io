29138a5c720fac38d71bbccf0397dfdf80bcd8a4
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 15:08:57 +0300

    Add files via upload

--- files/ps3/js/Far_Cry_2.js
+++ files/ps3/js/Far_Cry_2.js
@@ -1,8 +1,8 @@
 
 		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvRnYtNFZUM1J0b0FRWnc=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvX1FyZFZSdGdrT1kwM1E=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);	
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);	
--- files/ps3/js/Heavenly_Sword.js
+++ files/ps3/js/Heavenly_Sword.js
@@ -1,28 +1,28 @@
 
 		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvR0R1VHB5Mm5EUXVXOWc=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvN0dBSzF6YjBGYmY3NEE=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + (decoded02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvOHpjRWFzSURXLUdkQUE=";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly95YWRpLnNrL2QvSGVHWmpsQmJLZmlOdXc=";
 	var decoded04 = atob(str04);
-	var link04 = (decoded04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "aHR0cHM6Ly95YWRpLnNrL2QvTmZDaW9pdzZBelJiNmc=";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + (decoded05);
+	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
 		var str06 = "aHR0cHM6Ly95YWRpLnNrL2QveXgzYWJKcHBhZDN6cEE=";
 	var decoded06 = atob(str06);
-	var link06 = (decoded06);	
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
 	
 		var str07 = "aHR0cHM6Ly95YWRpLnNrL2QvWUFPU1pmNWlPS2VzYlE=";
 	var decoded07 = atob(str07);
-	var link07 = (decoded07);		
+	var link07 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str07);		
