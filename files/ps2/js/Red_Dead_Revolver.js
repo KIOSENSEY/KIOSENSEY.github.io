2d147b3e542ac2273a4be688f59b66c4f99aac02
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 16:50:01 +0300

    Add files via upload

--- files/ps2/js/Red_Dead_Revolver.js
+++ files/ps2/js/Red_Dead_Revolver.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0REcjVKR1NTLTk1eFZB";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps3/js/Call_of_Duty_Modern_Warfare_2.js
+++ files/ps3/js/Call_of_Duty_Modern_Warfare_2.js
@@ -9,5 +9,5 @@
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2NQWWE2ZkxOQlJWcTNRL01vZGVybiUyMFdhcmZhcmUlMjAyLjMucGtn";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://sh.st/st/1a8d994000ae273e2b83c0d8676f1bed/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
