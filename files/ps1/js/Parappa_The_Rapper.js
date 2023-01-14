4c79865092279476d49d83c991361a373b87b8c1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 13:15:07 +0300

    Add files via upload

--- files/ps1/js/Parappa_The_Rapper.js
+++ files/ps1/js/Parappa_The_Rapper.js
@@ -1,4 +1,4 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzlrTFFxcF9yV0o5amVR";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
