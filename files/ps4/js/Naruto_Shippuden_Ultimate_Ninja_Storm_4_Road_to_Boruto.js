b8149e457bf35f507f1d76f20cc139bd2b71825f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 14:11:32 +0300

    Add files via upload

--- files/ps4/js/Naruto_Shippuden_Ultimate_Ninja_Storm_4_Road_to_Boruto.js
+++ files/ps4/js/Naruto_Shippuden_Ultimate_Ninja_Storm_4_Road_to_Boruto.js
@@ -1,3 +1,3 @@
 		var str01 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBaGVqUmNwZFlsc2RnUi1VeGV3T3RZVF9uYXZZ";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
