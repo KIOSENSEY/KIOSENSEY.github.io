bff9c9d26cde22f310a091caf452a84c17f5f200
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-14 17:17:33 +0300

    Add files via upload

--- files/ps2/js/The_Sopranos_Road_to_Respect.js
+++ files/ps2/js/The_Sopranos_Road_to_Respect.js
@@ -1,3 +1,3 @@
 		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9EeDVqU0RUV2dhU1BmaWZTakRHTlR5UGd4ajgx";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps2/js/The_Suffering_Ties_That.js
+++ files/ps2/js/The_Suffering_Ties_That.js
@@ -1,3 +1,3 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTY1Njg0NDg/aGFzaD1ESjlPZ0I5RURCZVhJdGhqSUFnbndJc2x6YVVIMkY0b0l2cThIc21KRVRUJmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
