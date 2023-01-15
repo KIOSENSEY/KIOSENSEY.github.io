1996a31e49326dcf1dcc069a2f0fc50b20298ab0
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-15 14:00:36 +0300

    Add files via upload

---
+++ files/ps3/js/Monster_Madness_Grave_Danger.js
@@ -0,0 +1,23 @@
+		var str01 = "";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMjQ3NjQ3MD90b2tlbj1YcWtsZkdNZ3RZMG1wMFMmZmlsZW5hbWU9TW9uc3RlciBNYWRuZXNzIEdyYXZlIERhbmdlciBbQkxFUzAwMjU1XS5pc28=";
+	var decoded06 = atob(str06);
+	var link06 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
