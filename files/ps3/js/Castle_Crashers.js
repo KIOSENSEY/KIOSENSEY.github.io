fa8ea9c6ed5e78f6a02985070be245ba9bb678ff
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-15 12:12:32 +0300

    Add files via upload

--- files/ps3/js/Castle_Crashers.js
+++ files/ps3/js/Castle_Crashers.js
@@ -6,7 +6,7 @@
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc5NDg5ODU/aGFzaD0ydmpGeGFJejBGR2VTNjJlVkpoUnliaXpuWnowR2wwV1h0S0ZSMDd2eTMwJmRs";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
