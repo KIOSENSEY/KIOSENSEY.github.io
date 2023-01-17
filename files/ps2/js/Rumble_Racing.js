242909a4ca11c825d158ccec6f017ba0c5fe144e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-17 18:57:10 +0300

    Add files via upload

---
+++ files/ps2/js/Rumble_Racing.js
@@ -0,0 +1,3 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTgxMDA5NTc/aGFzaD1pbkxKSnpjeGx6T0NsTEI2WER1WWFsd1Q3R0Z1WDBYNGxkRldqd0lOTWJvJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
