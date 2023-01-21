1c06fc4b53165769d0dd7f4b197553d33792c7ff
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-21 18:56:35 +0300

    Add files via upload

--- files/ps3/js/Tomb_Raider_Underworld.js
+++ files/ps3/js/Tomb_Raider_Underworld.js
@@ -3,7 +3,7 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "https://disk.yandex.ru/d/FMbu7O0Ibdtz4g";
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0ZNYnU3TzBJYmR0ejRn";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
