989fefe943e8f3d308e80dcf769c59e8020119a3
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-21 20:26:35 +0300

    Add files via upload

--- files/ps3/js/Grand_Theft_Auto_Episodes_from_Liberty_City.js
+++ files/ps3/js/Grand_Theft_Auto_Episodes_from_Liberty_City.js
@@ -1,9 +1,9 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy84ZjVBQmhoN1loQjI4a3lYTjZaaFQ5aVJRU1pr";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTgzNjI0MTY/aGFzaD1IWHdReHZsTE5uTFhBUms1ZVdKclptb1RBQ1FNVWtLdk5UVVpiQ05Sakk4JmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9halk2VFROdVh1Znd5OVYzU1NqOFM3M0Q2U1Y2";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTgzNjMwNTc/aGFzaD1CS0x6WlFYeHZxREVwU0VReUkyeUlrcnVhcE94S3RPSXdzTVpmWmc0OWpUJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
