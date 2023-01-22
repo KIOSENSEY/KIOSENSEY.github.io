211b145d8b3fe7c6158febed87aa21d0559f8b6c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-22 12:54:57 +0300

    Add files via upload

--- files/ps3/js/Dantes_Inferno.js
+++ files/ps3/js/Dantes_Inferno.js
@@ -10,3 +10,7 @@
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL001UEd3WXpGUU1Id1dBL0RhbnRlJ3MlMjBJbmZlcm5vLjMucGtn";
 	var decoded03 = atob(str03);
 	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMzU5ODMyMT90b2tlbj1PRU1KZjBlSDRNYjh5SUQmZmlsZW5hbWU9RGFudGVzIEluZmVybm8uW0JMRVMwMDcxM11bQUxMX0RMQ10ucGtn";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);	
