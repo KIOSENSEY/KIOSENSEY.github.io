e3d32633a18b7bb18fb5d0d325fa1b418afd054e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-22 12:33:11 +0300

    Add files via upload

--- files/ps3/js/Transformers_War_for_Cybertron.js
+++ files/ps3/js/Transformers_War_for_Cybertron.js
@@ -1,15 +1,15 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9CVmk5VGc3a1p0RlROYjVZTmpEWVRRQldWQ1ND";
+		var str01 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMzU5MjIwMT90b2tlbj1JYmJYM3JpVDZpYldpTGMmZmlsZW5hbWU9VHJhbnNmb3JtZXJzIFdhciBmb3IgQ3liZXJ0cm9uLltCTEVTMDA4MzNdLlswMV0ucGtn";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9oeDgzeWkyRFlZNHpaYWdWWGgxUldCVnd4eWtG";
+		var str02 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMzU5NDA2MD90b2tlbj1hRHhCdUUyZUVpSThVUlEmZmlsZW5hbWU9VHJhbnNmb3JtZXJzIFdhciBmb3IgQ3liZXJ0cm9uLltCTEVTMDA4MzNdLlswMl0ucGtn";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9CVlB5dkF4d3hXajN3TnlpVjE5UTZiaWlHd2dW";
+		var str03 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMzU5NDI2MD90b2tlbj1xNHJqM2NrV2RSM2pwUVQmZmlsZW5hbWU9VHJhbnNmb3JtZXJzIFdhciBmb3IgQ3liZXJ0cm9uLltCTEVTMDA4MzNdLlswM10ucGtn";
 	var decoded03 = atob(str03);
 	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy82UGFGV1k1ekFRYmhoYTZGanZXQnVhdEdUYWp0";
+		var str04 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMzU5NDMxOT90b2tlbj1UcXNmcU1xWmtxU2t3TXUmZmlsZW5hbWU9VHJhbnNmb3JtZXJzIFdhciBmb3IgQ3liZXJ0cm9uLltCTEVTMDA4MzNdW1VQREFURV1bMDEuMDFdLnBrZw==";
 	var decoded04 = atob(str04);
 	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
