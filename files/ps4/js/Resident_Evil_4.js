8f2044c143fc2b86bae198eb9d0d3a2a2392cdb6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-23 09:30:59 +0300

    Add files via upload

--- files/ps4/js/Resident_Evil_4.js
+++ files/ps4/js/Resident_Evil_4.js
@@ -1,9 +1,8 @@
-
-		var str01 = "";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2xrNkNkV3hVTFhpQ21R";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3FkMXlOaW9zcmR1cU9n";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
@@ -13,12 +12,12 @@
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
-		var str06 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5jb20vZC9vY095bkFOeU5aZjhCQQ==";
+		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
