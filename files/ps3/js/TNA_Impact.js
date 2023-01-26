ab8e9f27fdd24c2bd601d320463126af25febf96
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-26 14:39:28 +0300

    Add files via upload

--- files/ps3/js/TNA_Impact.js
+++ files/ps3/js/TNA_Impact.js
@@ -1,5 +1,5 @@
 
-		var str01 = "aHR0cHM6Ly8xZHJ2LmRlL3UvcyFBaklBa3MtTWFHWTBpZ3NvbWNUdmdHb0Q2LTV4P2U=";
+		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL1MtcjVOamRUN19VZTBB";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
