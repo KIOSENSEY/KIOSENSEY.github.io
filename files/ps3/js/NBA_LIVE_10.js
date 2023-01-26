7079f61ff26f4c40a2ba980c185635df27e20c2a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-26 22:24:51 +0300

    Add files via upload

--- files/ps3/js/NBA_LIVE_10.js
+++ files/ps3/js/NBA_LIVE_10.js
@@ -8,7 +8,7 @@
 	
 		var str03 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xN054SG9TTW1nWE1mYmhNZmFoVWwycmxTMndXUTI5WHcvdmlldw==";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xREJDbkgzZVpLV0VFSjlJYnNUZkdIVmVYcUtuaV91TC0vdmlldw==";
 	var decoded04 = atob(str04);
