db7071080198e5961f0949b8bc0631e8504552a5
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-26 18:54:13 +0300

    Add files via upload

---
+++ files/ps4/js/Valfaris.js
@@ -0,0 +1,23 @@
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg2NzkyNzY/aGFzaD1jdmJDdGZFWFNaVVp1bFJyQnBOem96Y3ByVExjbEdqdTk5aGRKbG9HNUJMJmRs";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg2NzkzNTA/aGFzaD1EU0hMc3gzMHFKT0tEaWdCMUZCVUJsa3J6MzNKNUFlWjdZc3pSeTU1WDh6JmRs";
+	var decoded02 = atob(str02);
+	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "";
+	var decoded03 = atob(str03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
