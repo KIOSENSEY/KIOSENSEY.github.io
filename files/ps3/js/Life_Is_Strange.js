72fa57329da8a7327d9fcc2dd8d83137d3f8158f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-26 21:42:43 +0300

    Add files via upload

--- files/ps3/js/Life_Is_Strange.js
+++ files/ps3/js/Life_Is_Strange.js
@@ -9,7 +9,7 @@
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2xSemdISDJvUjlUR2FR";
 	var decoded03 = atob(str03);
-	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMTQzMzYwND90b2tlbj1sMUhLaU1IeXJYM2JlTzUmZmlsZW5hbWU9TGlmZSBpcyBTdHJhbmdlIFtOUEVCMDIxOTddIFJ1U291bmQgRXAuMS5wa2c=";
 	var decoded04 = atob(str04);
