a294ceeb8173d40ccd86044d83b1d53dfe2ef269
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-27 10:43:50 +0300

    Add files via upload

--- files/ps3/js/Final_Fantasy_XIII-2.js
+++ files/ps3/js/Final_Fantasy_XIII-2.js
@@ -4,7 +4,7 @@
 	
 		var str02 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xdHpIYWpRYW8tV2xTdkRTNm1IN3lYQ2g3T1RNcjJPNzk=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xeldnX0w1S2owVmUxR2dmcFJyLTJkU213NC00dlFSRTU=";
 	var decoded03 = atob(str03);
--- files/ps3/js/eFootball_2023.js
+++ files/ps3/js/eFootball_2023.js
@@ -4,7 +4,7 @@
 	
 		var str02 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMjY1NjI1Mz90b2tlbj02TzlVTGpRQU04Y25KMGMmZmlsZW5hbWU9ZUZvb3RiYWxsIDIwMjMgW0JMRVMwMjI1Ml1bRU5HJ1JVU10uWzAyXS5wa2c=";
 	var decoded02 = atob(str02);
-	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMjY1NjM3Nz90b2tlbj12a2ZzeGNpaGhkNGNmdjEmZmlsZW5hbWU9ZUZvb3RiYWxsIDIwMjMgW0JMRVMwMjI1Ml1bRU5HJ1JVU10uWzAzXS5wa2c=";
 	var decoded03 = atob(str03);
