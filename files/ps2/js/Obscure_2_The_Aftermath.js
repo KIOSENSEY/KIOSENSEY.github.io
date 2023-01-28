5e7db664db2a58b92830eb207ac9a950e34268b9
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-28 22:24:52 +0300

    Add files via upload

--- files/ps2/js/Obscure_2_The_Aftermath.js
+++ files/ps2/js/Obscure_2_The_Aftermath.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9aek5oMjVaM2JHUFNBNDZYdk51RlZoelR6aHg5";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg4MjU3NDc/aGFzaD1TcHFZbE1KQkUydmhaRURhZ1puVk9McG5WYVR2Mnh6UG04b2JVQm9PbDFjJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
