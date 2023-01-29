7c9732e174b88a912c4eb2778d9f54cbecf488ed
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-29 14:54:14 +0300

    Add files via upload

--- files/ps3/js/Dead_Space_3.js
+++ files/ps3/js/Dead_Space_3.js
@@ -18,3 +18,7 @@
 		var str05 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xU0k3WHcwU25hNkhuM1k2dVRqdGVNbnFPWkR3WDh0a2cvdmlldw==";
 	var decoded05 = atob(str05);
 	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg4NTMxNzY/aGFzaD1CRXZpY3ZVRXRsUkR3WGp5dTU2RkhaelBaUFY4aXpNWlpjSXdzYmZoanJYJmRs";
+	var decoded06 = atob(str06);
+	var link06 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
---
+++ files/ps4/js/Car_Mechanic_Simulator.js
@@ -0,0 +1,23 @@
+		var str01 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNDcwNzQ0Mj90b2tlbj1ZdWdJN1p5azl1a0RjNm4mZmlsZW5hbWU9Q2FyIE1lY2hhbmljIFNpbXVsYXRvciBbQ1VTQTE0NTI4XVtHQU1FXVswMS4wMV0ucGtn";
+	var decoded01 = atob(str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+		var str02 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNDcwODAwNz90b2tlbj15bXQ5cnZtS3g2b1pWZjEmZmlsZW5hbWU9Q2FyIE1lY2hhbmljIFNpbXVsYXRvciBbQ1VTQTE0NTI4XVtVUERBVEVdWzAxLjA3XS5wa2c=";
+	var decoded02 = atob(str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	
+		var str03 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNDcwODA0OT90b2tlbj1GanU0SkM0aHpYYnhnejImZmlsZW5hbWU9Q2FyIE1lY2hhbmljIFNpbXVsYXRvciBbQ1VTQTE0NTI4XVtETENdLnJhcg==";
+	var decoded03 = atob(str03);
+	var link03 = "http://q32.ru/65495/" "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
