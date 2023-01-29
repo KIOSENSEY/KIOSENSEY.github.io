89f573d66c42d82cca19e92b55aa3a2e37412f00
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-29 19:51:50 +0300

    Add files via upload

--- files/ps3/js/Apache_Air_Assault.js
+++ files/ps3/js/Apache_Air_Assault.js
@@ -1,4 +1,4 @@
-		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTczOTY1Nzk/aGFzaD12WVNWOHNabmF6MXJINHphbzhzdDBLVGFpZG1KUTM4RUhjSGZzSzRJajM0JmRs";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTczOTY1Nzk/aGFzaD1DSnFqakRYY0FLN00ya24xeHp1V2RmUGtQYTBGNDJpcW9idHA3RVNaSkRzJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
