1fad5b5db8e4562894a9cb8425ba6335e5a13f24
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-01-29 20:02:31 +0300

    Add files via upload

--- files/ps3/js/Batman_Arkham_City.js
+++ files/ps3/js/Batman_Arkham_City.js
@@ -9,3 +9,7 @@
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2JSM0JkSENHaDZDN3V3L0JhdG1hbiUyMEFya2hhbSUyMENpdHkuJTVCMDMlNUQucGtn";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
+	
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg4NzM3OTk/aGFzaD1abVpPYVk4OEM4dUp3QjdhejI0SzloZkx3ekk2bjdoeWJGSHgzNDhSbkNMJmRs";
+	var decoded04 = atob(str04);
+	var link04 = "http://q32.ru/65495/" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);	
