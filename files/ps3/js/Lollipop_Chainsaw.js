8f31977e059faee81985533cdda73322d90bd918
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-04 00:39:26 +0300

    Add files via upload

--- files/ps3/js/Lollipop_Chainsaw.js
+++ files/ps3/js/Lollipop_Chainsaw.js
@@ -1,8 +1,8 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0VfZm9WWk03b29Nc1R3L0wuQy4lMjAlNUIwMSU1RC5wa2c=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0VfZm9WWk03b29Nc1R3L0wuQy4lMjAlNUIwMiU1RC5wa2c=";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
--- files/ps3/js/Lost_Planet_Extreme_Condition.js
+++ files/ps3/js/Lost_Planet_Extreme_Condition.js
@@ -1,16 +1,16 @@
 
 		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvREg2WUExREtKYjBtckE=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvNXgxLUItZnJjZU1SRXc=";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvVVRXY3NicnJ0alYwM0E=";
 	var decoded03 = atob(str03);
-	var link03 = "http://q32.ru/65495/" + (decoded03);
+	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "aHR0cHM6Ly95YWRpLnNrL2QvbEYtN1dhc0NlMGJPcmc=";
 	var decoded04 = atob(str04);
-	var link04 = (decoded04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
