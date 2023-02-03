593af94aa6442ad99c82ad8a713e43dc41deed96
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-03 15:28:01 +0300

    Add files via upload

--- files/ps3/js/The_Lord_of_the_Rings_Aragorns_Quest.js
+++ files/ps3/js/The_Lord_of_the_Rings_Aragorns_Quest.js
@@ -1,23 +1,23 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1NjUyNDc/aGFzaD1udGVCN3Z5UDQ0T2xYQ055Tk5COW5HSDdodXIzemNwY2hkbGR6dk5xWmd3JmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1NjU2MTc/aGFzaD1JQkFmR0pEMmtEWU1WWG5USHVlVVAyT3ZxaUxDQzVFUXdLbkE3TXN3aTFnJmRs";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1NjU3NDg/aGFzaD1SbU1GdURMY0IyTVZOUHNKRG1ZOGdhakNDWTQyTmlGb1R6dmZwa29xNGQwJmRs";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = (decoded05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+	var link06 = (decoded06);
