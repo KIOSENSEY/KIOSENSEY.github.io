47a4a1a7bf4c58c965f8f395122948c8b32efd5a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-03 14:56:48 +0300

    Add files via upload

--- files/ps3/js/Ski-Doo_Snowmobile_Challenge.js
+++ files/ps3/js/Ski-Doo_Snowmobile_Challenge.js
@@ -1,23 +1,23 @@
 		var str01 = "";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
 	
 		var str03 = "";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = (decoded03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = (decoded05);
 	
 		var str06 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzMjQ3NzA1MT90b2tlbj1zVkVRdG05NlVIaVV6clAmZmlsZW5hbWU9U2tpLURvb64gU25vd21vYmlsZSBDaGFsbGVuZ2UgW0JMRVMwMTQ4Nl0uaXNv";
 	var decoded06 = atob(str06);
-	var link06 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+	var link06 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://q32.ru/65495/" + (decoded06);
