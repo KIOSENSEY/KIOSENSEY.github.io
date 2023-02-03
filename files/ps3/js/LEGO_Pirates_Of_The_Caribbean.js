6b35ddd78d06e474544c7f0506652eff9bd401cc
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-03 13:26:55 +0300

    Add files via upload

--- files/ps3/js/LEGO_Pirates_Of_The_Caribbean.js
+++ files/ps3/js/LEGO_Pirates_Of_The_Caribbean.js
@@ -1,24 +1,24 @@
 
 		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvRDJCd05Sc3paNzJtRVE=";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvTnFVRW0zcnR2WkVKTEE=";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvdzVDT2QzcHNLUDM0S0E=";
 	var decoded03 = atob(str03);
-	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://q32.ru/65495/" + (decoded03);
 	
 		var str04 = "aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBb3Y3NjlOWXZqYUdnVnVCdEY5Yzl4X3FydW5T";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://q32.ru/65495/" + (decoded05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
+	var link06 = (decoded06);	
--- files/ps3/js/inFamous.js
+++ files/ps3/js/inFamous.js
@@ -1,25 +1,25 @@
 
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0l2ZFotb0xSNjJaVnlnL0kuRi4lNUIwMSU1RC5wa2c/dz0x";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
 	
 		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0l2ZFotb0xSNjJaVnlnL0kuRi4lNUIwMiU1RC5wa2c/dz0x";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0l2ZFotb0xSNjJaVnlnL0kuRi4lNUIwMyU1RC5wa2c/dz0x";
 	var decoded03 = atob(str03);
-	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://q32.ru/65495/" + (decoded03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = (decoded04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	var link05 = "http://q32.ru/65495/" + (decoded05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+	var link06 = (decoded06);
 	
