a19971005d1de91c4e163c27b8fe7451cc57f791
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-04 11:02:23 +0300

    Add files via upload

--- files/ps3/js/LittleBigPlanet_3.js
+++ files/ps3/js/LittleBigPlanet_3.js
@@ -1,17 +1,17 @@
 
-		var str01 = "aHR0cHM6Ly95YWRpLnNrL2QvSzhDN2M0eDVCTzhoOVE=";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTkyMTE1NjE/aGFzaD1EbXBkTDBGaldGQUljcFpHV3E0Vmh5YlBGYW8wTUp0eWxnMG1zbW13dmtnJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvSVJRV0lpem1SMG96aUE=";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTkyMTI5NDk/aGFzaD1QVDM5bUdRbWlFYUlrcVN6Z1AzVkV0VFRpOUUxbDE5UnlZc3J5Z3E2YzdEJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvVmQ0WFVIUDRPb2VjVmc=";
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTkyMTM0NzQ/aGFzaD1DUU9VR1ZUazBObENuS3ZLTFVLeUVyMVk5SzRjc09vUFQ3emdoNHg1Z1p3JmRs";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly95YWRpLnNrL2Qvd1JlMm5BdzdTZkdEM0E=";
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTkyMTM2NzA/aGFzaD1nMGt1dTNYN253Q1R6Zzd0Y1Y5WkZqekhLWGpXQk8yWmp1WGVvOXNDOW44JmRs";
 	var decoded04 = atob(str04);
 	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
