c3cdbcd22679b70fbf85c2b800587a727a40e2ea
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-04 10:41:25 +0300

    Add files via upload

--- files/ps3/js/The_Swapper.js
+++ files/ps3/js/The_Swapper.js
@@ -1,23 +1,23 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTgwNzY1MTQ/aGFzaD1iS2RiOGMycE03Y1NyQW9ZWGFFSEVjY3VaR0xVN2ttaDVtQzVqR2M1Zjg4JmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
 		var str03 = "";
 	var decoded03 = atob(str03);
-	var link03 = (decoded03);
+	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
-	var link05 = (decoded05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
 	
 		var str06 = "";
 	var decoded06 = atob(str06);
-	var link06 = (decoded06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
--- files/ps3/js/Tom_Clancys_Splinter_Cell_Double_Agent.js
+++ files/ps3/js/Tom_Clancys_Splinter_Cell_Double_Agent.js
@@ -1,7 +1,7 @@
 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg0MzM0ODU/aGFzaD04d0FaU2Q3ZEFMOEwyVEI0WTByTjFjcll6SlVGMFNKYVFPYjBQWnlib2NnJmRs";
 	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + (decoded01);
+	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTg1MTk3NjQ/aGFzaD15ZE9XTHJqdFIxVVhYVGg5TFZrSUx4cWhsekJOeG9ueGdmRHlUUG42bVdnJmRs";
 	var decoded02 = atob(str02);
-	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + (decoded02);
+	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
