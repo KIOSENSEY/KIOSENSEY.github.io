edd6b959f7b9f13ce7ef7c4625be3b9b089a8b71
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-08 09:52:58 +0300

    Add files via upload

--- files/ps3/js/Army_of_Two_The_40th_Day.js
+++ files/ps3/js/Army_of_Two_The_40th_Day.js
@@ -1,12 +1,12 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8zUzExODZmWGo2U2ZpaERXUk5ZTkJqVjY2R050";
+		var str01 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNjA5MzEwOT90b2tlbj03VmVGMUJxbk82VVNLR0wmZmlsZW5hbWU9QXJteSBvZiBUV08gVGhlIDQwdGggRGF5IFtCTEVTMDA2NTldWzAxXS5wa2c=";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9aQlBEdHlRdTVDakF6VjhDenc0UGk0ellBeUY5";
+		var str02 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNjA5MzExNz90b2tlbj12WHpQR0QzTkN6NXVJQ20mZmlsZW5hbWU9QXJteSBvZiBUV08gVGhlIDQwdGggRGF5IFtCTEVTMDA2NTldWzAyXS5wa2c=";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9nWE5EU2loNzJaeFdEOVR2eGZrQVNGMzE1UGJ2";
+		var str03 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQzNjA5MjU0Nz90b2tlbj1ETlpoRm1zaWh1aWJkR1omZmlsZW5hbWU9QXJteSBvZiBUV08gVGhlIDQwdGggRGF5IFtCTEVTMDA2NTldWzAzXS5wa2c=";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
