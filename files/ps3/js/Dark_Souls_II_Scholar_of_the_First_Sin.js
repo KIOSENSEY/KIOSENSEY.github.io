dddbbe1195827c3d7b710ccc73ca1a8e16842873
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-09 17:29:38 +0300

    Add files via upload

--- files/ps3/js/Dark_Souls_II_Scholar_of_the_First_Sin.js
+++ files/ps3/js/Dark_Souls_II_Scholar_of_the_First_Sin.js
@@ -1,13 +1,13 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8xWTUzQTlCalNXRmJmOTd5YWJ5OWl0aTVnOGJ4";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk1MjkzODA/aGFzaD1Eelk3UkRiYnllNGM5d2R6djJLUFhZdnZjZ3ZsczdlanFsV0RyeEpxekdnJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy96dWo2WloyWHoxMjhQTnZrYXhhU3poWDlhYkdH";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk1MzAwOTU/aGFzaD0xUlFCcVNXeXVtZW1ZOXVTeUZCWlhTMFlMUmRVT0FuTndJYUdWQ21Vc2RIJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9CYmpUVms5Wmt5aVM1NVJid1ZRTmlXQTJGa1BB";
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL0tYRzFuYmF4Wk5ZdmFB";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
