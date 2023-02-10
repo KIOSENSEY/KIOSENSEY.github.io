3874129ce83cb9d12ac6d56d3467f7f181889571
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-10 10:47:29 +0300

    Add files via upload

--- files/ps3/js/Bloodbath.js
+++ files/ps3/js/Bloodbath.js
@@ -1,3 +1,11 @@
 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL183cXhWM1JiRERZSTJ3";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk1NDE0OTI/aGFzaD12UkRmaXVNWjBrQno0WEs4QWhJSVRVQzh1OFdsUEt0QUdVWEZ0bXA0RjJMJmRs";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
