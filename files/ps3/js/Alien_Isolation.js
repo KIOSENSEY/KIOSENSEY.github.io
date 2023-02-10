a32b0de4283f1d3ffe4233ef01d32a2b5a95cf6e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-10 15:12:00 +0300

    Add files via upload

--- files/ps3/js/Alien_Isolation.js
+++ files/ps3/js/Alien_Isolation.js
@@ -8,4 +8,8 @@
 	
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3lGUWp2NFBZdm51YjZnL0FsaWVuX0lzb2xhdGlvbi4lNUIwMyU1RC5wa2c=";
 	var decoded03 = atob(str03);
-	var link03 = "http://q32.ru/65495/" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
+	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
+	
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1MzkwNzgyOT9oYXNoPUdoQzR5Rmo1eTVxNG93MUU4bzQydUJGdXpSQjMwUHJpT3Z4NEp6a0F1TnomZGw=";
+	var decoded04 = atob(str04);
+	var link04 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);		
