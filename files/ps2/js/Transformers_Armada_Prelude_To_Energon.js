af14eecc615798269594549551c9c897538f1bd1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-13 19:24:30 +0300

    Add files via upload

--- files/ps2/js/Transformers_Armada_Prelude_To_Energon.js
+++ files/ps2/js/Transformers_Armada_Prelude_To_Energon.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9zNS51cGZpbGVzLmRvd25sb2FkLzIwMjMvMDIvMTIvMTAvOUo3NFVDMDdTZ29LejJXcml1dTRaQmcxV2RvZTM3YkV1Ym5xTFhYUS5wa2c/bmFtZT0lNUJQUzIlNURUcmFuc2Zvcm1lcnMrQXJtYWRhK1ByZWx1ZGUrVG8rRW5lcmdvbi4lNUJTTEVTNTIzODglNUQucGtnJmlwPTk0LjE5LjUyLjQxJm1kNT1ZaUlhaVJ6VTFSYy1JTVdWTmhNbVFBJmV4cGlyZXM9MTY3NjMxNzYyMQ==";
+		var str01 = "aHR0cHM6Ly92ay5jYy9jbHlUbGg=";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
