431817371346b216f666df1bf8f4afe14e8e6533
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-13 10:49:35 +0300

    Add files via upload

--- files/ps2/js/Transformers_Armada_Prelude_To_Energon.js
+++ files/ps2/js/Transformers_Armada_Prelude_To_Energon.js
@@ -1,4 +1,4 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yWlg4VjRqalJ0dWE0UlJ0a2lOZkZmUHl0U3c0";
+		var str01 = "aHR0cHM6Ly9zNS51cGZpbGVzLmRvd25sb2FkLzIwMjMvMDIvMTIvMTAvOUo3NFVDMDdTZ29LejJXcml1dTRaQmcxV2RvZTM3YkV1Ym5xTFhYUS5wa2c/bmFtZT0lNUJQUzIlNURUcmFuc2Zvcm1lcnMrQXJtYWRhK1ByZWx1ZGUrVG8rRW5lcmdvbi4lNUJTTEVTNTIzODglNUQucGtnJmlwPTk0LjE5LjUyLjQxJm1kNT1ZaUlhaVJ6VTFSYy1JTVdWTmhNbVFBJmV4cGlyZXM9MTY3NjMxNzYyMQ==";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
--- files/ps3/js/Saw.js
+++ files/ps3/js/Saw.js
@@ -3,7 +3,7 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk3MzgyNDM/aGFzaD02VVgyR3NMYmkxck14c0R5bklEYnJ6QzBFdHdOblZXMEdCQ0NQUGQ5d1FjJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
