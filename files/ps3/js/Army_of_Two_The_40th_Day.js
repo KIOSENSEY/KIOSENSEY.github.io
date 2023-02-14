eaf6c242cbb04e24138a02d2733cc300d15de5c1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-14 12:00:17 +0300

    Add files via upload

--- files/ps3/js/Army_of_Two_The_40th_Day.js
+++ files/ps3/js/Army_of_Two_The_40th_Day.js
@@ -10,7 +10,7 @@
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "";
+		var str04 = "aHR0cHM6Ly92ay5jYy9jbHpYckE=";
 	var decoded04 = atob(str04);
 	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
