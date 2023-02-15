25e8cc339c8d204dba07e74e801b608d9e236a2f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-15 17:30:35 +0300

    Add files via upload

---
+++ files/ps2/js/Shinobi.js
@@ -0,0 +1,8 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDIwOTc0Mj9oYXNoPU1LbU9YME1CNzV6R1BsNmNadjQ2bFEwendIU25oeUNPNEQxVjlRWDM5WnMmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDIxMDcxOD9oYXNoPWhBd3ZUVmR3THlVN2l2eXpBcDlpalNUZXg0bm41ZFpnUzFmVXpnOXloSWcmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
