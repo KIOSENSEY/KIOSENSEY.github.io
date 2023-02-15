98977cd823f8f64b60e0b56ea09d99010d6ad42f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-15 21:46:12 +0300

    Add files via upload

--- files/ps2/js/Pirates_of_the_Caribbean_The_Legend_of_Jack_Sparrow.js
+++ files/ps2/js/Pirates_of_the_Caribbean_The_Legend_of_Jack_Sparrow.js
@@ -1,4 +1,8 @@
-
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy8yaDFQNGF6elhZTlNEQ2gzR1RTMjVXaTN3Q0Ri";
-	var decoded01 = atob(str01);
-	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDIzNDAyNz9oYXNoPURXVnNnTnRNN2UxWWxKUjJSU0psV3p6SmRTQUtId0NYR1c2N1ZxcTdadTQmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 =  "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDIzNDM4MT9oYXNoPTZxTWgxVnV6Q3pvSk1KY3V5c3dZQWo1QWZ3dEk2amRZMkt1OUxxb08zamMmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
