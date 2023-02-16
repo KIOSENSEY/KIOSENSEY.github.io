bd5db2446946fc634ee16ce3401e499345cd41b8
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-16 19:31:15 +0300

    Add files via upload

--- files/ps3/js/Cabelas_Survival_Shadows_of_Katmai.js
+++ files/ps3/js/Cabelas_Survival_Shadows_of_Katmai.js
@@ -1,7 +1,7 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9UQ3ZrYmJXYVlRdEJQZmZCYVRDODRneE5RWFpS";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk5NjIxNDQ/aGFzaD03ZzFoUzA2bjhvUlVBWVZZeEpxRDM5S1ltZHozQk9YOWs5OUVrOG1UTXpzJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy94NkQ0Q0JHWXo2eldnYXdYODlpOHZXaHlRNFI0";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTk5NjM4NzQ/aGFzaD12cENoOW5pWllYS1Rkd0tGOWliS1R0SXRvV29MQm91WXMxbDVWUmxmT01UJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
