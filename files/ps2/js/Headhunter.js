e1860ac26c549807eb5b74d45cdd0992cfa8f603
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-17 15:45:02 +0300

    Add files via upload

---
+++ files/ps2/js/Headhunter.js
@@ -0,0 +1,12 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMyOTEwMD9oYXNoPUNReXUwVnl4djJSWjRTc3hQMVlwU0RoMjBhOHF1SFhFRXh4ekcyeWphT1QmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMzMDE4OT9oYXNoPVRDVlZaOGdLUjh6WkY3bzVLNDJDcFNabHpZS1R0enhXcDI0VjlOMG1jOUwmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 =  "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMzMDI3Nz9oYXNoPURLM2hIZldpenZPc1RZd2ViVXJPaXJEMlF5ekNuT2VIYjRWcjYzRDBHc0wmZGw=";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+ 	
