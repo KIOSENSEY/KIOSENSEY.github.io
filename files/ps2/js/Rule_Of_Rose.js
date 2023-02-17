f1eaf805ee6d83668831cfc8f53f79e1989fbe25
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-17 10:53:35 +0300

    Add files via upload

---
+++ files/ps2/js/Rule_Of_Rose.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMxMDg1Nz9oYXNoPVR2R25sWkRvVDVDa2J0VEswVnVKbXVBeHpSR0NldU5BZmZvSnB4akVhSFAmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
