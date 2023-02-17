17949c3dcc6757c79548d29cada340e153e14569
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-17 11:02:07 +0300

    Add files via upload

--- files/ps3/js/The_Club.js
+++ files/ps3/js/The_Club.js
@@ -3,7 +3,7 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "https://disk.yandex.ru/d/nnw6jdoyYaW5dA";
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL25udzZqZG95WWFXNWRB";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
