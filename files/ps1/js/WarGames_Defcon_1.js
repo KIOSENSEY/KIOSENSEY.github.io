f62a1f49afe26efd99bc31b517f0e5d02e743fae
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-17 12:29:47 +0300

    Add files via upload

---
+++ files/ps1/js/WarGames_Defcon_1.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMxNzc0MD9oYXNoPVdiUEZ5eVpQSE5tWVB6WmxDelZNN3lERlMyc2NlZVhVT0M1UVVyRGlyZjQmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
