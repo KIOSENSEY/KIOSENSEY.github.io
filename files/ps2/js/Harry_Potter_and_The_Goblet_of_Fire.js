ebc72cce912259467ba30336483e7cc8a60bd5e9
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-17 14:06:21 +0300

    Add files via upload

---
+++ files/ps2/js/Harry_Potter_and_The_Goblet_of_Fire.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMyNDI3NT9oYXNoPXNoRTI1OEg2QnpEdWZ3b0RiZnZlQ3F1STNPdXUwbEVkUm5uOEtsazZJMHMmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
