d72e29b7a76addd1ce58c72f99c981b9765aea9a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-18 17:32:01 +0300

    Add files via upload

---
+++ files/ps2/js/Syphon_Filter_Logans_Shadow.js
@@ -0,0 +1,12 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDM5MDc4Nz9oYXNoPUVvNW1lSVpwaldhS1pYcmRWTDNsbU1CVTZKQU05VkZSTWJldjAzU0xac2MmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDM5MTExMj9oYXNoPTRVazdWekpmUFRHV2Y2Zm9RWkdWRVhiVXdybm1pRnhtWVQ3WDJWWnpaT0gmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDM5MTE4Mj9oYXNoPTNUUEIxaDlaZHFnUk9QdmxzbWFwejBOdkltOVdveHhra2xzYzBSNTFMbEQmZGw=";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+ 	
