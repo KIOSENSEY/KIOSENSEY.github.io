7c6fcf8560a920a41dca4e603f6ccebb64d3d35e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-18 10:05:42 +0300

    Add files via upload

---
+++ files/psp/js/Toy_Story_3.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAwNDg2MjU/aGFzaD1aTG5SaUVVQ0N4alAwenpvN2VBYzNGWlpYa3R6MXhFcldzTTRBTWdabzgwJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
