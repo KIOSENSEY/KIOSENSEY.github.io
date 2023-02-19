5027bb941cf882831c57915cace773cd473a613b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-19 12:34:04 +0300

    Add files via upload

---
+++ files/ps2iso/js/Teenage_Mutant_Ninja_Turtles_Smash-Up.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAwOTIzMzM/aGFzaD1VaGhqU0E5WXRFV09nNUtnTHNkaXpBM01vWno0OHBNdTM3empzWHg1T3lIJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/The_Mummy_Returns.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAxMTAwMzU/aGFzaD1zbGk5eTlQSDJmZEZwWnNGckR0ejFUaUtrWTlwTk4ybGtNa1JEU3hLTXF3JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
