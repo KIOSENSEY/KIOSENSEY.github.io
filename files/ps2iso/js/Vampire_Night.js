b90f061e967a982d558b63490e1ecc2bff332c43
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-19 16:12:22 +0300

    Add files via upload

---
+++ files/ps2iso/js/Vampire_Night.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAxMTY1MDk/aGFzaD1KcTgzc1lWR1hFakh4c2FNWmw5Y01yblB4SHpERU1Fd0dPeVV4QmQzSnpvJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
