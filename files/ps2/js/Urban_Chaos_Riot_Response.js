ec1ef180f07f8b30633a0ee9df2b3c7b9b46bf9e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-21 17:17:57 +0300

    Add files via upload

---
+++ files/ps2/js/Urban_Chaos_Riot_Response.js
@@ -0,0 +1,12 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNTAwNDg/aGFzaD0zVnpGTVAyMHFNRjNNWm1aUzNOUjc4TENYQmpHSzJudTVKc1hGMGpjRm9YJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNTA0MDE/aGFzaD01Ymk2ZU5hZlQ1ZFN5NWhTYzFlb0hMNVIwU1BVZnRvazFuelJ5cEVIRWU0JmRs";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNTA1NzI/aGFzaD0xTFNLU2J1UDIwZ0xxTkFXNndQVGhNd1hWaWxHcFExT2QyWnJ4bXBxenFrJmRs";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+ 	
