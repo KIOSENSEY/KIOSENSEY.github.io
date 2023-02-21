158d33a3648e2b833665ea76c5801396ff239d9c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-21 11:28:24 +0300

    Add files via upload

---
+++ files/psp/js/LEGO_Indiana_Jones_The_Original_Adventures.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAxNzY0MDc/aGFzaD1VdVMwOVJVRjExcTdORGhxaXFpNGRscThSS1ZSS09LR0l1S0tKbzJRU1R3JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/psp/js/LEGO_Pirates_of_the_Caribbean_The_Video_Game.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAxNzQ2NDg/aGFzaD1ENlNCUHNFYnRvZm1WWnA3QUl0UHd0b0dPYW1CZDFCOEpJcno4eDloWGQ0JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
