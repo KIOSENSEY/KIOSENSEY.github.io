5c449af18732759c48d26ec0bc24f1e63198159a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-21 20:18:30 +0300

    Add files via upload

---
+++ files/ps2/js/Dead_Or_Alive_2_Hardcore.js
@@ -0,0 +1,12 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNjcwMzk/aGFzaD1nbWFyZXROQkpGNDJaR1Z0ZG52TmdoSGxqQmdGUFo4b3ZTVE1vM01sb1RIJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNjc1MTg/aGFzaD1GOWc1c0tINVdLRHhlTXcwZ24wWlVEWUNLM1RpTGFCY1NJQ2g1R3VXN3kwJmRs";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyNjc2NTE/aGFzaD1VcXJXUlZqWlZNWDVzZTdFTk9DNEdhdnp3Zmt6YmVKcFcwdTVvOUp4TmlYJmRs";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+ 	
