ceeb5cb6410af8434cd80b288e5c4c717c26d8e7
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-21 12:48:52 +0300

    Add files via upload

--- files/ps3/js/Dungeon_Siege_III_Treasures_of_the_Sun.js
+++ files/ps3/js/Dungeon_Siege_III_Treasures_of_the_Sun.js
@@ -1,12 +1,12 @@
 
-		var str01 = "aHR0cHM6Ly9vZC5say9kL09EWmZNekk1TWprd01EZGYvRHVuZ2VvbiUyMFNpZWdlJTIwSUlJJTIwUlVTJTVCcHNob21lLm9yZyU1RC5pc28=";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyMzUzODI/aGFzaD1WWUd5OVpOSkt3OUF2bnlHR0RFVGZXZ2VNSHJ0YUc5c1FpdVd6N3dkSmowJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9vZC5say9kL09EWmZNekk1TWpnNU5qVmYvRHVuZ2VvbiUyMFNpZWdlJTIwSUlJJTIwUGF0Y2glMjAxLjAzJTIwUlVTJTVCcHNob21lLm9yZyU1RC5wa2c=";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyMzU2MTA/aGFzaD1RdHZvQzloZ3JsejZROEg2bW9kd3hHQnVyTXpjek12MzhnMGR1UUZ3ZmtzJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9vZC5say9kL09EWmZNekk1TWpnNU56RmYvRHVuZ2VvbiUyMFNpZWdlJTIwSUlJJTIwLSUyMFRyZWFzdXJlcyUyMG9mJTIwdGhlJTIwU3VuJTIwRExDJTIwUlVTJTVCcHNob21lLm9yZyU1RC5wa2c=";
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyMzU2Njc/aGFzaD0xQzJaOGRtTXhZbkc1YmZvSENxV0lEenZWZ0N6ejg0YnFCdnRzcmpNYU00JmRs";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
