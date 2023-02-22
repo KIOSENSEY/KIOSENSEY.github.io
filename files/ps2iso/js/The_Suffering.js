f94a27a5e58a84525380fcfe59facace481a1fe0
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-22 11:44:47 +0300

    Add files via upload

---
+++ files/ps2iso/js/The_Suffering.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAyOTE0NDA/aGFzaD11cVdoRld2YXgxbEVnTDRpTFRCWlJJTURxTE5MOTkyVkkyQ0RteHpVYmdnJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
