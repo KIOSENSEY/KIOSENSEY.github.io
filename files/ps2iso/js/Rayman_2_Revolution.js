eb339ee7776b039d4565364926db37eeed7d3027
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-24 12:44:06 +0300

    Add files via upload

---
+++ files/ps2iso/js/Rayman_2_Revolution.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjAzNjAzMzY/aGFzaD01TFZaN2hSNE5IQUJIMWVVaGd5TkJhOTd2UGNPV0VWYXc3WmtvcHBiU3BjJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
