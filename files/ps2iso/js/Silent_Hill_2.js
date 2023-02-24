f99c9ae16ac4abcaa1d6660bea3d063e8231828c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-24 15:27:04 +0300

    Add files via upload

---
+++ files/ps2iso/js/Silent_Hill_2.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA0MjU1ODY/aGFzaD1iQjRDTzFkd0l0TFlITGRlTXBUM2VOQkRrQk9XVW83THBjaXhpUEJrbmRMJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/Soldier_of_Fortune_Gold_Edition.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA0MjA4Mzk/aGFzaD1ySlptNk5aT2ZxU293SFVOd1ZTVWg3VHpYRnowWGJLOUh6YVlaeFl3ckU4JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
