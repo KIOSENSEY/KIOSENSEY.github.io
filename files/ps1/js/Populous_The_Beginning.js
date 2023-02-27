79ebcfe8f906f1eacdd52ff18eec804ebd3bf28c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-27 09:55:31 +0300

    Add files via upload

--- files/ps1/js/Populous_The_Beginning.js
+++ files/ps1/js/Populous_The_Beginning.js
@@ -1,4 +1,4 @@
- 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMxODY0Mz9oYXNoPXpyYXJVZno2eDAyVlZyd3JyYjhleUJpU3RZTlVQTkRVcGFlajZid0NZWFgmZGw=";
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDg5ODA5Nj9oYXNoPWJaS1dSTklscUh6YjhCWFV5dGVpOFg4Z0hDbk1rWGV1NFhXaWY5VHhvOUgmZGw=";
  	var decoded01 = atob(str01);
  	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
  	
--- files/ps1/js/WarGames_Defcon_1.js
+++ files/ps1/js/WarGames_Defcon_1.js
@@ -1,4 +1,4 @@
- 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDMxNzc0MD9oYXNoPVdiUEZ5eVpQSE5tWVB6WmxDelZNN3lERlMyc2NlZVhVT0M1UVVyRGlyZjQmZGw=";
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NDg5ODE0Nz9oYXNoPTBKMGpjaVJDR3lvcGZRZjJUODdMSzlUUVA4ZzF1eEZFTU5GWGFIelk2VFQmZGw=";
  	var decoded01 = atob(str01);
  	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
  	
