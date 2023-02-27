a93d41f2756dcc2d817b79579894d8cd8ed5340c
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-27 15:03:41 +0300

    Add files via upload

---
+++ files/ps2iso/js/Call_of_Duty_World_at_War_Final_Fronts.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA1MzcxODQ/aGFzaD1KejdyNUNVZ2J4UEZwTmtaa2QxR0YyVTZPM0x5RmQ5OGpvb245cUdxdkJEJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/The_Mummy_Tomb_of_the_Dragon_Emperor.js
@@ -0,0 +1,7 @@
+
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA1MzgwNTk/aGFzaD1lRVpWYjRObjJ6REtpV3EyaWZZR1NEYkVGTEJvZFptVEZCeWF2SGg2cmpvJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 	
+ 	
