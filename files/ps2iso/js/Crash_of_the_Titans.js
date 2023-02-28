69729799edfa0d74a772ece30923199637799e49
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-02-28 14:44:21 +0300

    Add files via upload

---
+++ files/ps2iso/js/Crash_of_the_Titans.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA2NTk3Njc/aGFzaD1tWjZ4N29JUUE1QzF0N2NWeld6dmFsNlNXVHlsNFp5MjRoR252Slk4M3FIJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/Manhunt_2.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA2NjA5ODI/aGFzaD04eXpobThTT1NsVDV6R2N1SnZwdFdLSUN1R21yNXBaWjJhTVM3VDVsM3RnJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
