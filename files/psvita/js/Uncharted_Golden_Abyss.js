12459930d073b4ed51143c652482cb914275ca0a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-01 15:08:59 +0300

    Add files via upload

---
+++ files/psvita/js/Uncharted_Golden_Abyss.js
@@ -0,0 +1,12 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDU1MT9oYXNoPXlvUzlWSkJ6cUVrSnpWTlRjQkUyZWl5aVlQWlFBMjN6Z0twcnJVNlZXZjQmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDk0OT9oYXNoPVZYdjgzeDlIb2xVemdQQmROcWVONUF6TWpaMEpYNU9ySXV0QzV3OVR1NGcmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNTEzMj9oYXNoPVNnTWhaWVZYTVVoRzBCMFFhZ2w1STdKb0xzTGg2cTBBaGZnc2xCbld4THomZGw=";
+ 	var decoded03 = atob(str03);
+ 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+ 	
