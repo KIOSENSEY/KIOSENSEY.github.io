640ace6a998592c74569b53bf10401cdaa2e527b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-01 10:21:09 +0300

    Add files via upload

---
+++ files/ps2/js/Peter_Jacksons_King_Kong.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjA3MTcxNzU/aGFzaD1VYUJUOW1Nb0k4QWtndjFmZmp6SUV6dFJaMlRWbjE2dnF6alVLaE96TzFEJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
