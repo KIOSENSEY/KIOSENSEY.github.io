10e05b0c74b6712d61b0f29e74a3e27811ac9843
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-02 23:33:00 +0300

    Add files via upload

--- files/ps3/js/Fight_Night_Champion.js
+++ files/ps3/js/Fight_Night_Champion.js
@@ -10,3 +10,20 @@
 		var str03 = "aHR0cHM6Ly95YWRpLnNrL2QvaDRYNHd4aDJpSlhtaGc=";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+	 		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTEzNDUyOD9oYXNoPVlLa3h6cDBDMFVEMDdLUDg5Zms0cXVncWJDZmJRMk5iaVRvaVlCV2VBamsmZGw=";
+ 	var decoded04 = atob(str04);
+ 	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+ 	
+ 		var str05 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTEzNDkzMT9oYXNoPXJnN082YWJiUmV2TmlQR0FLUXpwNUFTdUhnV2FXZkpQS3pWdTRremkybGMmZGw=";
+ 	var decoded05 = atob(str05);
+ 	var link05 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+ 	
+ 		var str06 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTEzNTAwMz9oYXNoPVp2UXZsbEIzZGVvVmJwV2U0dDcxUFl3UWpMaVV6UFVFam5nZW9WZXFabmsmZGw=";
+ 	var decoded06 = atob(str06);
+ 	var link06 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);
+ 	
+ 		var str07 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTEzNTAxOT9oYXNoPTZ6VFJkZmIxSWFDQUNiM0d6WGo0dE5KVVZYVExtOVJiYUdLMXBObFBjbGsmZGw=";
+ 	var decoded07 = atob(str07);
+ 	var link07 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str07);
+ 	
