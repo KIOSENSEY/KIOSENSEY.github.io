937cd8e8ee39be1432e288368231466c8308b101
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-03 11:48:22 +0300

    Add files via upload

---
+++ files/ps2/js/50_Cent_Bulletproof.js
@@ -0,0 +1,8 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTE1MDM4OT9oYXNoPXJqaXhNdlp5ampyUkd5ejcwVFhJb0txVWpEejJrMVdJbjgxd1ZvWkhDRjQmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTE1MDcwMT9oYXNoPWdsbm9MMmp2YVRoc2VwbHR3Z1RPM0I2TGpucFRmZmJ5YldvbllqVmZ5SGcmZGw=";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
