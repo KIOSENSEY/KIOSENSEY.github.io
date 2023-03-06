3cff41a94cfbade5acbbb57d8bd44185a1656009
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-06 20:30:43 +0300

    Add files via upload

---
+++ files/retro/js/TIME_RETRO_GAMES.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTM1ODAzMj9oYXNoPTJLbEpyaGlGbnFaZ2ZmTUt5ODVmYjFteE9OSjlxbHRLWjBLSzFhNE13TzQmZGw=";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
