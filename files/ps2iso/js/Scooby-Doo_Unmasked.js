8ffcacd14a8a6c92f57adac23267cc51001ebac4
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-07 14:56:05 +0300

    Add files via upload

---
+++ files/ps2iso/js/Scooby-Doo_Unmasked.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjEwODk5NjA/aGFzaD1IcVVBeU5pSWtPWEQ5aE9OalR5V1o3N1JlbXdmWjF0UU02enZpVG9FRzVMJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
