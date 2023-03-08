a4179191385097ae1fd06e2fa8644d6d0fdf2a48
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-08 11:53:11 +0300

    Add files via upload

--- files/ps3/js/UNDERTALE.js
+++ files/ps3/js/UNDERTALE.js
@@ -1,5 +1,5 @@
 
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9BanROOVBhM1JYWU5rOU4yRDlGYlp5TnVrazZS";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTQ1MTkxNj9oYXNoPXRHRDZKQjdjenBDZzNkVU1QOFdGRndYRGJQSXIwa2hHdFJWdGRJaTRHOWcmZGw=";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
