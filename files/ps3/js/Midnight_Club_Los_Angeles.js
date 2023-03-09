9e157182183ea30a4e160e9f0223a544eb30e88b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-09 11:49:22 +0300

    Add files via upload

--- files/ps3/js/Midnight_Club_Los_Angeles.js
+++ files/ps3/js/Midnight_Club_Los_Angeles.js
@@ -7,13 +7,13 @@
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTM4NjI2MD9oYXNoPU9RZWJKcnNaV0FnaGVKNXNPM1RMOHBxSUVQQzFLSjR1bXI4dVJ6SnMwdjgmZGw=";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "";
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTM4NjM0OT9oYXNoPU9jVjRaMlNIRG8zUVM4SDBZeVhXZFNJd3JGSlJkTVhab2xQT1VtUlhoQzQmZGw=";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
