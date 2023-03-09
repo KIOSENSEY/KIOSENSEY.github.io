f4930997c4b21adb7044290604789d3c91e83d73
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-09 11:01:14 +0300

    Add files via upload

---
+++ files/ps2iso/js/The_Lord_of_the_Rings_The_Return_of_the_King.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjEyMDYzOTY/aGFzaD1QbU5SZzBVcVN6RWQ2OEN4YmowRDM0M2gxR2R1VU8xb2RtOGRlMk4wZFNUJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
