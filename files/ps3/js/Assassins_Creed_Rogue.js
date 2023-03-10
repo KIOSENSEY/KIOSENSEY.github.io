a0daad5d35f9d26033be73cd1b99c6d3dad0e6ed
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-10 21:42:15 +0300

    Add files via upload

--- files/ps3/js/Assassins_Creed_Rogue.js
+++ files/ps3/js/Assassins_Creed_Rogue.js
@@ -1,11 +1,11 @@
-		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODc1NDE/aGFzaD1xdXM2eHhlSVMxeE1xdFpSWTQ2OHFPUVJ6UlpnaHpCdkZSZW1zcmJzQldzJmRs";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODc1NDE/aGFzaD02dXRyaUlTMlNkMm1oWDJHMTBaMXRqb0V6V2xoOUF5bzZraTJpNTNEenlrJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODgyMDE/aGFzaD1QM25ldE1EUnV4M1dLQ2hlSmRoOUVPeG43YndtVGdqYnZxSWt2UlB5YWJEJmRs";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODgyMDE/aGFzaD03TzhhQVlGVVNEMDVRZ0VkdFBWbnp2Yno3aDR2NlBNRjNaWkE0bVB0NUtzJmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODgyMzA/aGFzaD1iclZFSmd6c1NLbFcwaWhRTHJOdEU3enpDTFJJS0x4WlFJZ3RBdjd0RW1vJmRs";
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NTc1ODgyMzA/aGFzaD1xNVJpekI0R29ZRHBTU2s3cHhmNDdUM3V2RHUxZFZORm5OSWVxdjROZzY0JmRs";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);	
