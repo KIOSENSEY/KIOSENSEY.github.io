dbc7ff97aa553b99e2c08df154e15744ad777ca0
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-13 13:41:58 +0300

    Add files via upload

---
+++ files/ps2iso/js/Stuntman_Ignition.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjEyODc0Nzc/aGFzaD1oWFprd3V6T1JBYlFveGZ6bnVZU1ZtQk9BWGhOaE9sNHl1WVhObkNaTXNQJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
