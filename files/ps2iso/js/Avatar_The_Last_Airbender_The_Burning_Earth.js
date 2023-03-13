64596039e72cecb6ffa19b96d27c872cd5a13185
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-13 11:49:43 +0300

    Add files via upload

---
+++ files/ps2iso/js/Avatar_The_Last_Airbender_The_Burning_Earth.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjEyODMyMjU/aGFzaD1DYUNwZ0s5Y1p5SjM4ZVVTelNNbmROb0pjTjdNRVo4MUR5bkNVcXk2Z1VjJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/Godzilla_Unleashed.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjEyODExOTQ/aGFzaD0wNHVJNVRJbGRaSnpMbXJhVjQ0blptY2VjZ2xzbkt4WjgwaXBWOThsNU1YJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
