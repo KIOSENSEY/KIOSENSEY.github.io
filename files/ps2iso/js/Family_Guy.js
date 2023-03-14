f25e08ca7d7211e7b7041712518b3ea4f6c388b5
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-14 14:52:54 +0300

    Add files via upload

---
+++ files/ps2iso/js/Family_Guy.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE0ODExODc/aGFzaD1uWjlJQkRrYlNmbmdvVlpmSExjSjNKTExiVlFKeU5uY3RHeklOTkZDdVFnJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
---
+++ files/ps2iso/js/Zombie_Hunters.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE0ODIzOTY/aGFzaD10Vmk2M1hMSXpXdzZQalVOdXl5YU1OMzhUTDJzbGhIS2RMWTlsTnBJUjljJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
