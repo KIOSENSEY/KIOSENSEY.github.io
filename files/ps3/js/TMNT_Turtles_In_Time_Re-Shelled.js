9a0419b487c9be23e5d900767c4248aab36839e1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-15 12:53:51 +0300

    Add files via upload

---
+++ files/ps3/js/TMNT_Turtles_In_Time_Re-Shelled.js
@@ -0,0 +1,8 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE1Njc3OTc/aGFzaD1DNmpxbFVaZ0JZd1V6YVA3RlpzYjFWcnFDSlB2bGlaRXlvZVZrQWN1eTcwJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE1Njc4MDE/aGFzaD1VYTIzS0lnMUYwYzBjQk9BMzNCazdaNmliTGF6Z21HaUxRTnFFRDkwT2NjJmRs";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
