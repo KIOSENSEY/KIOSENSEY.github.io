7bf6569532ca5a130551c9127b265269b57ff2a1
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-16 13:39:36 +0300

    Add files via upload

---
+++ files/ps3/js/HARD_CORPS_UPRISING.js
@@ -0,0 +1,8 @@
+ 		var str01 = "LINKERSING";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
+ 		var str02 = "LINKERSING";
+ 	var decoded02 = atob(str02);
+ 	var link02 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
+ 	
