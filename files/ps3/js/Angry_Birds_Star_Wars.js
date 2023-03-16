bcffc205aced1342cd72c38e29e388c16de9416e
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-16 13:07:09 +0300

    Add files via upload

---
+++ files/ps3/js/Angry_Birds_Star_Wars.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE2MzM4MDQ/aGFzaD1aTVgzN21DWTVNTzZDZmlyRU1rek9PeU9WZUNMcXdQMHloRHhkSlQ4Ulo4JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
