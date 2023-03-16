a30b1dbb324de7bd688c7068815f5c3164be1800
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 01:38:19 +0300

    Add files via upload

--- files/psvita/js/Minecraft_PlayStation_Vita_Edition.js
+++ files/psvita/js/Minecraft_PlayStation_Vita_Edition.js
@@ -1,4 +1,4 @@
  		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE1NjIwOTA/aGFzaD1Nd2FianBTVTQyaUpBb0FTUnZaR2RsMmd6bWx2RHJ1TXpjVU5oeWo1MFNEJmRs";
  	var decoded01 = atob(str01);
- 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	var link01 = "http://ouo.io/qs/FZviLabz?s=" + "http://ccl.su/go.php?h_i=12735&h_u=" + (str01);
  	
