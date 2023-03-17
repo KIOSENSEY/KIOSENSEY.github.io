4fe18224c6b25f76c44acf9901bac26263ebf07b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 09:17:50 +0300

    Add files via upload

--- files/psvita/js/Uncharted_Golden_Abyss.js
+++ files/psvita/js/Uncharted_Golden_Abyss.js
@@ -1,12 +1,12 @@
- 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDU1MT9oYXNoPXlvUzlWSkJ6cUVrSnpWTlRjQkUyZWl5aVlQWlFBMjN6Z0twcnJVNlZXZjQmZGw=";
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDU1MT9oYXNoPUdWcWJsUDVqTmRXeGZVT2QyUXlNSEg0YWVYNGF2alpTT1l3TmNhY3E1NVAmZGw=";
  	var decoded01 = atob(str01);
  	var link01 = "http://ouo.io/qs/FZviLabz?s=" + "http://ccl.su/go.php?h_i=12735&h_u=" + (str01);
  	
- 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDk0OT9oYXNoPVZYdjgzeDlIb2xVemdQQmROcWVONUF6TWpaMEpYNU9ySXV0QzV3OVR1NGcmZGw=";
+ 		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNDk0OT9oYXNoPVhheFdKWWpGM0FHZEJRWndNTGFTSzlGVEM4bENnWENEeXlaY0E1ellwNVgmZGw=";
  	var decoded02 = atob(str02);
  	var link02 = "http://ouo.io/qs/FZviLabz?s=" + "http://ccl.su/go.php?h_i=12735&h_u=" + (str01);
  	
- 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNTEzMj9oYXNoPVNnTWhaWVZYTVVoRzBCMFFhZ2w1STdKb0xzTGg2cTBBaGZnc2xCbld4THomZGw=";
+ 		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTAzNTEzMj9oYXNoPWVoQk5kVDc1b2hlVFpycTJIMHNvVlAzZTUzYkF6NkljRzlaTHkyaElLTTQmZGw=";
  	var decoded03 = atob(str03);
  	var link03 = "http://ouo.io/qs/FZviLabz?s=" + "http://ccl.su/go.php?h_i=12735&h_u=" + (str01);
  	
