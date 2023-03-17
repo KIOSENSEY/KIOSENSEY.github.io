87bbb6694b889fddb4d43392d04c2a1b9f2283bd
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 14:51:08 +0300

    Add files via upload

--- files/ps3/js/Duke_Nukem_Forever.js
+++ files/ps3/js/Duke_Nukem_Forever.js
@@ -11,6 +11,6 @@
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3ZJNkRIZUtiSnh6OFd3";
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE2Mzk4MDM/aGFzaD12VmRJR1dOeUNiZUR3Rldsd3hweU8yOXZXRGhMUEtJdTEwUXplRWc5SE1zJmRs";
 	var decoded04 = atob(str04);
 	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);	
