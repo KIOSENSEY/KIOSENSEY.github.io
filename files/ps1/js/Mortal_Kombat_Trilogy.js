5e8cbadc46cd563a66ce9657323176dc331020b9
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 19:57:31 +0300

    Add files via upload

--- files/ps1/js/Mortal_Kombat_Trilogy.js
+++ files/ps1/js/Mortal_Kombat_Trilogy.js
@@ -1,4 +1,4 @@
- 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jNTI1ODY5NTkxXzY1NTM2MDM0MD9oYXNoPXB6cDdFZk9aZnk5QmNiNnpZblFCVlVYTTRVbU9XTFBRNmVRdkozMjEwcHMmZGw=";
+ 		var str01 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3ppX09RX0pETTRhT2hB";
  	var decoded01 = atob(str01);
  	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
  	
