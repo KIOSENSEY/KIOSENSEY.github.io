1007c5aff6167d5ea93916b4c7501b9a29608d8a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 11:49:03 +0300

    Add files via upload

---
+++ files/psvita/js/Lego_Ninjago_Nindroids.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE2OTQ3NDY/aGFzaD1la0xGYzBWMFN2VzNYSFZCWUo0ekZ1UHg3dnd4T0xkTE1aSGxLMERTZDhEJmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
