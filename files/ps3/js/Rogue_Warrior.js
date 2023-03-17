4c988031cb293c9572b21ce5bff5457175d134d0
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-17 20:08:15 +0300

    Add files via upload

--- files/ps3/js/Rogue_Warrior.js
+++ files/ps3/js/Rogue_Warrior.js
@@ -3,7 +3,7 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly9kb3dubG9hZC5zYmVyZGlzay5ydS9kb3dubG9hZC9maWxlLzQ0MDYxNzc1Mz90b2tlbj1va3FsRmVQYWczS1NpYVImZmlsZW5hbWU9Um9ndWUgV2Fycmlvci5bQkxFUzAyMTM2XVtFTkddLmlzbw==";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
