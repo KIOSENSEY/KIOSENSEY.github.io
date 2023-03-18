7421f69adf3b2f9661a5c267f23ea400698e1700
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-18 11:30:04 +0300

    Add files via upload

--- files/ps3/js/Monsters_vs_Aliens.js
+++ files/ps3/js/Monsters_vs_Aliens.js
@@ -7,9 +7,9 @@
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly93ZWIub3BlbmRyaXZlLmNvbS9hcGkvdjEvZG93bmxvYWQvZmlsZS5qc29uL09EWmZNekl3T1RnMk1qSmY/aW5saW5lPTA=";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
