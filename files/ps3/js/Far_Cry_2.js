12ab8e0a4762a231ce613876a78ab9f0acf7906b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-18 15:26:22 +0300

    Add files via upload

--- files/ps3/js/Far_Cry_2.js
+++ files/ps3/js/Far_Cry_2.js
@@ -6,3 +6,11 @@
 		var str02 = "aHR0cHM6Ly95YWRpLnNrL2QvX1FyZFZSdGdrT1kwM1E=";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);	
+	
+		var str03 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE3NTI3Mzg/aGFzaD1tckwxVVQ2NDY2RHhuUFVENmNqT29UeTFCVHpDc2cxdTB0OHBXWHhqTGlrJmRs";
+	var decoded03 = atob(str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE3NTI3NDY/aGFzaD1jQXozcklyanJlRk5iMkZYYXp6bTJmcEVEUGV5SmdTSkRDb2hOekg0TVJ3JmRs";
+	var decoded04 = atob(str04);
+	var link04 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);		
