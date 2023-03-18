d2f3bbe33ec3c0e3eea60162d41079fc66dc6651
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-18 22:01:48 +0300

    Add files via upload

--- files/ps3/js/Call_of_Duty_World_at_War.js
+++ files/ps3/js/Call_of_Duty_World_at_War.js
@@ -10,3 +10,8 @@
 		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzdwMVVJR0ZBTkZWd21BL0NhbGwlMjBvZiUyMER1dHklMjBXb3JsZCUyMGF0JTIwV2FyJTIwJTVCMDMlNUQucGtn";
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	
+		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5jb20vZC9VQWVDbHl6MWt3R2JBUQ==";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
