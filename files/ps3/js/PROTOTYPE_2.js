e27a6ef163e3868e7dd52583cc4de72a93543af6
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-19 15:41:28 +0300

    Add files via upload

--- files/ps3/js/PROTOTYPE_2.js
+++ files/ps3/js/PROTOTYPE_2.js
@@ -7,13 +7,13 @@
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kLzJGdXVvSmpfekh3T0Z3";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
-	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
 	
 		var str05 = "";
 	var decoded05 = atob(str05);
