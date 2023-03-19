f469aa10ffd0ccb984e1b8baddab7f99fe94eba7
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-19 12:34:59 +0300

    Add files via upload

--- files/ps3/js/Dead_Island.js
+++ files/ps3/js/Dead_Island.js
@@ -11,3 +11,7 @@
 	var decoded03 = atob(str03);
 	var link03 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
+		var str04 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2lad256MmFJRW9uLTBB";
+	var decoded04 = atob(str04);
+	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
