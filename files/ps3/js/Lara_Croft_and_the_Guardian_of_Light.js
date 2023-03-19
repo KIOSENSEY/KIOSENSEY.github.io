1537be1706bf66a088f6201ab158a85a1f2fe79b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-19 14:31:23 +0300

    Add files via upload

--- files/ps3/js/Lara_Croft_and_the_Guardian_of_Light.js
+++ files/ps3/js/Lara_Croft_and_the_Guardian_of_Light.js
@@ -1,8 +1,8 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9HMzIzWTkxNmpoVmY4UjZpZ3lUOFI2aXd1MkIy";
+		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE4MDAxMDk/aGFzaD03TUU4ajhYb3BwbWtVMnpvNE5OMWRtQ3FtREJ5UDhOS05DRkRpempmUDVEJmRs";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9nNXZhUXVTTjJnNjJiQlEzM1Z0amt1YnY0a3U0";
+		var str02 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE4MDA2MDE/aGFzaD00aHdCY2hPSlpZUDlPQVljZ3VJMnE2ZkFUWXB0bEZTNnBFbTV6bENBWTl6JmRs";
 	var decoded02 = atob(str02);
 	var link02 = "https://exe.io/st?api=2ed10d749840c3b77d1a23c29947d39c83882ac6&url=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
