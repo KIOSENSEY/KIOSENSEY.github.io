2ceb9807e9309da0610fef246fad263bf29c132f
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-21 10:09:07 +0300

    Add files via upload

---
+++ files/ps1/js/Worms_Armageddon.js
@@ -0,0 +1,4 @@
+ 		var str01 = "aHR0cHM6Ly92ay5jb20vZG9jLTE5Njc2NjQzM182NjE5MDE0NzA/aGFzaD16VnZ3OVdFMW1GUEVCU3JPMmdzMzFPWjBiNVFlWmRZUmQ2OHdWUjlNY3F3JmRs";
+ 	var decoded01 = atob(str01);
+ 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
+ 	
