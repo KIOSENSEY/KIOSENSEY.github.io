cc1c9e37e7699caa9430b29bee71a5d632a64409
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-21 14:03:19 +0300

    Add files via upload

--- files/ps3/js/KILLZONE.js
+++ files/ps3/js/KILLZONE.js
@@ -3,7 +3,7 @@
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "";
+		var str02 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL3RlR1ltdjBjc2ZMcUV3";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
