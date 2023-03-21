4fadc4038957e32b05202ea7d7d242d0b78735d5
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-21 11:53:21 +0300

    Add files via upload

--- files/ps3/js/WWE_2K16.js
+++ files/ps3/js/WWE_2K16.js
@@ -1,15 +1,23 @@
-		var str01 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9XdmdpdVpmOUdoQ0Y5NmZ1dmZRUHp1UVRoMzJS";
+		var str01 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xLV90MkZsT1RIY2RabDdDR2FvdUtSMXpOZHJBb3Ftd3QmZXhwb3J0PWRvd25sb2Fk";
 	var decoded01 = atob(str01);
 	var link01 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str01);
 	
-		var str02 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9RNEdGMnQ3TjZYVzFSOXdUOWp5dTg0U2k1QlR1";
+		var str02 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xNlNOMk9nTWhQdVhibGFEeWxuQmNFaWxOTG1oS0pTTGomZXhwb3J0PWRvd25sb2Fk";
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9qYmZaWGZRdFZ6QlhUaGsxWkd5dmtZZlQxYno5";
+		var str03 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xZjNfRi1mbGJKcTF2QzFCZXlqY1loUUd5b2pOQi1BeTgmZXhwb3J0PWRvd25sb2Fk";
 	var decoded03 = atob(str03);
 	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
-		var str04 = "aHR0cHM6Ly9pY2Vkcml2ZS5uZXQvcy9YeXRqMnpOamtYR1o0Q1BDWTM4eDNHYjR5NEI2";
+		var str04 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xMVBRak1zRDdzdzU0R2FxRnRIMGlZRzZjOWRoc3BpUEgmZXhwb3J0PWRvd25sb2Fk";
 	var decoded04 = atob(str04);
-	var link04 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	var link04 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str04);
+	
+		var str05 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xamExWTU3eXZkRER1QTJWTzk4aWZ3aUV4VHMxUGZhc1UmZXhwb3J0PWRvd25sb2Fk";
+	var decoded05 = atob(str05);
+	var link05 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str05);
+	
+		var str06 = "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL3UvMC91Yz9pZD0xOFZKMTEwNTJRakx4UTIxUGRYM2UwWlFOVE16SjFOREUmZXhwb3J0PWRvd25sb2Fk";
+	var decoded06 = atob(str06);
+	var link06 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str06);	
