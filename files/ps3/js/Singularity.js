67269f7aa617ecb38d0376a95b24feda0b83d85b
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-03-22 13:33:22 +0300

    Add files via upload

--- files/ps3/js/Singularity.js
+++ files/ps3/js/Singularity.js
@@ -7,9 +7,9 @@
 	var decoded02 = atob(str02);
 	var link02 = "http://q32.ru/65495/" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str02);
 	
-		var str03 = "";
+		var str03 = "aHR0cHM6Ly9kaXNrLnlhbmRleC5ydS9kL2JEVWhrd3BzcVdFVnVB";
 	var decoded03 = atob(str03);
-	var link03 = "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
+	var link03 = "http://ouo.io/qs/iCKfnfRr?s=" + "http://catcut.net/go.php?h_i=12735&h_u=" + (str03);
 	
 		var str04 = "";
 	var decoded04 = atob(str04);
