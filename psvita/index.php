3a3ccbecb355595b45bed29b5e4476f72c095c8a
KIOSENSEY <60900040+KIOSENSEY@users.noreply.github.com>
2023-08-27 11:08:32 +0300

    Add files via upload

---
+++ psvita/index.php
@@ -0,0 +1,87 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/php/vars_ban.php";
+?>
+<html>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<title>Скачать игры PlayStation Vita</title>
+<link rel="stylesheet" href="/css/style.css">
+<link rel="shortcut icon" href="/img/main/psv.png">
+	<body>
+		<div class="head_block">
+		<div id="head_block_in"></div>
+	</div>					
+		</div>
+		<div class="left_block">
+		<div id="left_block_in"></div>
+		</div>
+		<div class="right_block">
+		<div id="right_block_in"></div>
+		</div>			
+		<div class="search_block">
+		<input class="input_searsh" type="text" id="mySearch" onkeyup="myFunction()" placeholder="Поиск Игры..." title="Скачать Введите название Игры для поиска"></input>
+		<br>
+		<div class="name"><h2>PlayStation® Vita</h2></div>
+		</div>
+		<div class="center_block">
+		<div class="center_block_in" id="scroll" >	
+ 	<b><a href="/psvita/games/Uncharted_Golden_Abyss.php"><img class="icon_psvita" src="/img/icon/psvita/Uncharted_Golden_Abyss.jpg" alt="button" title="Скачать Uncharted™: Golden Abyss / Uncharted™: Золотая Бездна"></a></b>
+	<b><a href="/psvita/games/The_Binding_of_Isaac_Rebirth.php"><img class="icon_psvita" src="/img/icon/psvita/The_Binding_of_Isaac_Rebirth.jpg" alt="button" title="Скачать The Binding of Isaac: Rebirth"></a></b>
+ 	<b><a href="/psvita/games/Minecraft_PlayStation_Vita_Edition.php"><img class="icon_psvita" src="/img/icon/psvita/Minecraft_PlayStation_Vita_Edition.jpg" alt="button" title="Скачать Minecraft PlayStation®Vita Edition"></a></b>
+ 	<b><a href="/psvita/games/Toukiden_The_Age_Of_Demons.php"><img class="icon_psvita" src="/img/icon/psvita/Toukiden_The_Age_Of_Demons.jpg" alt="button" title="Скачать Toukiden: The Age Of Demons"></a></b>
+ 	<b><a href="/psvita/games/Plants_vs_Zombies.php"><img class="icon_psvita" src="/img/icon/psvita/Plants_vs_Zombies.jpg" alt="button" title="Скачать Plants vs. Zombies"></a></b>
+ 	<b><a href="/psvita/games/Child_of_Light_Complete_Edition.php"><img class="icon_psvita" src="/img/icon/psvita/Child_of_Light_Complete_Edition.jpg" alt="button" title="Скачать Child of Light: Complete Edition"></a></b>
+ 	<b><a href="/psvita/games/LittleBigPlanet_PS_Vita_Marvel_Super_Hero_Edition.php"><img class="icon_psvita" src="/img/icon/psvita/LittleBigPlanet_PS_Vita_Marvel_Super_Hero_Edition.jpg" alt="button" title="Скачать LittleBigPlanet: PS Vita Marvel Super Hero Edition"></a></b>
+ 	<b><a href="/psvita/games/The_Muppets_Movie_Adventures.php"><img class="icon_psvita" src="/img/icon/psvita/The_Muppets_Movie_Adventures.jpg" alt="button" title="Скачать The Muppets: Movie Adventures"></a></b>
+ 	<b><a href="/psvita/games/SpongeBob_HeroPants.php"><img class="icon_psvita" src="/img/icon/psvita/SpongeBob_HeroPants.jpg" alt="button" title="Скачать SpongeBob HeroPants"></a></b>
+ 	<b><a href="/psvita/games/Lego_Ninjago_Nindroids.php"><img class="icon_psvita" src="/img/icon/psvita/Lego_Ninjago_Nindroids.jpg" alt="button" title="Скачать Lego Ninjago: Nindroids"></a></b>
+ 	<b><a href="/psvita/games/Batman_Arkham_Origins_Blackgate.php"><img class="icon_psvita" src="/img/icon/psvita/Batman_Arkham_Origins_Blackgate.jpg" alt="button" title="Скачать Batman: Arkham Origins Blackgate"></a></b>
+ 	<b><a href="/psvita/games/Call_of_Duty_Black_Ops_Declassified.php"><img class="icon_psvita" src="/img/icon/psvita/Call_of_Duty_Black_Ops_Declassified.jpg" alt="button" title="Скачать Call of Duty: Black Ops Declassified"></a></b>
+ 	<b><a href="/psvita/games/Mortal_Kombat_9_Komplete_Edition.php"><img class="icon_psvita" src="/img/icon/psvita/Mortal_Kombat_9_Komplete_Edition.jpg" alt="button" title="Скачать Mortal Kombat 9: Komplete Edition"></a></b>
+ 	<b><a href="/psvita/games/Attack_on_Titan_Wings_of_Freedom.php"><img class="icon_psvita" src="/img/icon/psvita/Attack_on_Titan_Wings_of_Freedom.jpg" alt="button" title="Скачать Attack on Titan: Wings of Freedom"></a></b>
+ 	<b><a href="/psvita/games/XCOM_Enemy_Unknown_Plus.php"><img class="icon_psvita" src="/img/icon/psvita/XCOM_Enemy_Unknown_Plus.jpg" alt="button" title="Скачать XCOM: Enemy Unknown Plus"></a></b>
+ 	<b><a href="/psvita/games/The_Amazing_Spider-Man.php"><img class="icon_psvita" src="/img/icon/psvita/The_Amazing_Spider-Man.jpg" alt="button" title="Скачать The Amazing Spider-Man"></a></b>
+ 	<b><a href="/psvita/games/Need_for_Speed_Most_Wanted.php"><img class="icon_psvita" src="/img/icon/psvita/Need_for_Speed_Most_Wanted.jpg" alt="button" title="Скачать Need for Speed: Most Wanted"></a></b>
+ 	<b><a href="/psvita/games/MUD_FIM_Motocross_World_Championship.php"><img class="icon_psvita" src="/img/icon/psvita/MUD_FIM_Motocross_World_Championship.jpg" alt="button" title="Скачать MUD FIM Motocross World Championship"></a></b>
+ 	<b><a href="/psvita/games/Sly_Cooper_Thieves_in_Time.php"><img class="icon_psvita" src="/img/icon/psvita/Sly_Cooper_Thieves_in_Time.jpg" alt="button" title="Скачать Sly Cooper Thieves in Time"></a></b>
+	<b><a href="/psvita/games/Rayman_Origins.html" ><img class="icon_psvita" src="/img/icon/psvita/Rayman_Origins.jpg" title="Скачать Rayman Origins"></a></b>
+	<b><a href="/psvita/games/Rayman_Legends.html" ><img class="icon_psvita" src="/img/icon/psvita/Rayman_Legends.jpg" title="Скачать Rayman Legends"></a></b>
+	<b><a href="/psvita/games/Duke_Nukem_3D_Megaton_Edition.html" ><img class="icon_psvita" src="/img/icon/psvita/Duke_Nukem_3D_Megaton_Edition.jpg" title="Скачать Duke Nukem 3D: Megaton Edition"></a></b>
+	<b><a href="/psvita/games/Steins_Gate.html" ><img class="icon_psvita" src="/img/icon/psvita/Steins_Gate.jpg" title="Скачать Steins Gate"></a></b>
+	<b><a href="/psvita/games/Ninja_Gaiden_Sigma_Plus.html" ><img class="icon_psvita" src="/img/icon/psvita/Ninja_Gaiden_Sigma_Plus.jpg" title="Скачать Ninja Gaiden Sigma Plus"></a></b>
+	<b><a href="/psvita/games/Worms_Evolution_Extreme.html" ><img class="icon_psvita" src="/img/icon/psvita/Worms_Evolution_Extreme.jpg" title="Скачать Worms Evolution Extreme"></a></b>
+	<b><a href="/psvita/games/BitDungeon.html" ><img class="icon_psvita" src="/img/icon/psvita/BitDungeon.jpg" title="Скачать BitDungeon"></a></b>
+	<b><a href="/psvita/games/Sonic_and_All-star_Racing_Transformed.html" ><img class="icon_psvita" src="/img/icon/psvita/Sonic_and_All-star_Racing_Transformed.jpg" title="Скачать Sonic and All-star Racing Transformed"></a></b>
+	<b><a href="/psvita/games/Injustice_Gods_Among_Us_-_Ultimate_Edition.html" ><img class="icon_psvita" src="/img/icon/psvita/Injustice_Gods_Among_Us_-_Ultimate_Edition.jpg" title="Скачать Injustice: Gods Among Us - Ultimate Edition"></a></b>
+	<b><a href="/psvita/games/God_of_War_Collection.html" ><img class="icon_psvita" src="/img/icon/psvita/God_of_War_Collection.jpg" title="Скачать God of War: Collection"></a></b>
+	<b><a href="/psvita/games/Machinarium.html" ><img class="icon_psvita" src="/img/icon/psvita/Machinarium.jpg" title="Скачать Machinarium"></a></b>
+	<b><a href="/psvita/games/Terraria.html" ><img class="icon_psvita" src="/img/icon/psvita/Terraria.jpg" title="Скачать Terraria"></a></b>
+	<b><a href="/psvita/games/Hotline_Miami_2.html" ><img class="icon_psvita" src="/img/icon/psvita/Hotline_Miami_2.jpg" title="Скачать Hotline Miami 2"></a></b>
+	<b><a href="/psvita/games/Don't_Starve_Giant_Edition.html" ><img class="icon_psvita" src="/img/icon/psvita/Don't_Starve_Giant_Edition.jpg" title="Скачать Don't Starve: Giant Edition"></a></b>
+	<b><a href="/psvita/games/Angry_Birds_Trilogy.html" ><img class="icon_psvita" src="/img/icon/psvita/Angry_Birds_Trilogy.jpg" title="Скачать Angry Birds: Trilogy"></a></b>
+	<b><a href="/psvita/games/Dead_Nation.html" ><img class="icon_psvita" src="/img/icon/psvita/Dead_Nation.jpg" title="Скачать Dead Nation"></a></b>
+	<b><a href="/psvita/games/LEGO_Marvel's_Avengers.html" ><img class="icon_psvita" src="/img/icon/psvita/LEGO_Marvel's_Avengers.jpg" title="Скачать LEGO: Marvel's Avengers"></a></b>
+	<b><a href="/psvita/games/Limbo.html" ><img class="icon_psvita" src="/img/icon/psvita/Limbo.jpg" title="Скачать Limbo"></a></b>
+	<b><a href="/psvita/games/Army_Corps_of_Hell.html" ><img class="icon_psvita" src="/img/icon/psvita/Army_Corps_of_Hell.jpg" title="Скачать Army Corps of Hell"></a></b>
+	<b><a href="/psvita/games/LEGO_Star_Wars_The_Force_Awakens.html" ><img class="icon_psvita" src="/img/icon/psvita/LEGO_Star_Wars_The_Force_Awakens.jpg" title="Скачать LEGO: Star Wars The Force Awakens"></a></b>
+	<b><a href="/psvita/games/The_LEGO_Movie_-_Videogame.html" ><img class="icon_psvita" src="/img/icon/psvita/The_LEGO_Movie_-_Videogame.jpg" title="Скачать The LEGO Movie - Videogame"></a></b>
+	<b><a href="/psvita/games/Hitman_GO_Definitive_Edition.html" ><img class="icon_psvita" src="/img/icon/psvita/Hitman_GO_Definitive_Edition.jpg" title="Скачать Hitman GO: Definitive Edition"></a></b>
+	<b><a href="/psvita/games/Borderlands_2.html" ><img class="icon_psvita" src="/img/icon/psvita/Borderlands_2.jpg" title="Скачать Borderlands 2"></a></b>
+	<b><a href="/psvita/games/Bridge_Constructor.html" ><img class="icon_psvita" src="/img/icon/psvita/Bridge_Constructor.jpg" title="Скачать Bridge Constructor"></a></b>
+	<b><a href="/psvita/games/Killzone_Mercenary.html" ><img class="icon_psvita" src="/img/icon/psvita/Killzone_Mercenary.jpg" title="Скачать Killzone: Mercenary"></a></b>
+	<b><a href="/psvita/games/WRC_4_FIA_World_Rally_Championship.html" ><img class="icon_psvita" src="/img/icon/psvita/WRC_4_FIA_World_Rally_Championship.jpg" title="Скачать WRC 4: FIA World Rally Championship"></a></b>
+	<b><a href="/psvita/games/Little_Deviants.html" ><img class="icon_psvita" src="/img/icon/psvita/Little_Deviants.jpg" title="Little Deviants"></a></b>
+	<b><a href="/psvita/games/Tearaway.html" ><img class="icon_psvita" src="/img/icon/psvita/Tearaway.jpg" title="Скачать Tearaway"></a></b>
+	<b><a href="/psvita/games/PlayStation_All-Stars_Battle_Royale.html" ><img class="icon_psvita" src="/img/icon/psvita/PlayStation_All-Stars_Battle_Royale.jpg" title="Скачать PlayStation All-Stars Battle Royale"></a></b>
+	<b><a href="/psvita/games/Rogue_Legacy.html" ><img class="icon_psvita" src="/img/icon/psvita/Rogue_Legacy.jpg" title="Скачать Rogue Legacy"></a></b>
+
+	<br>
+	<br>
+	</div>
+	</div>
+	</div>
+		<div class="lower_block">
+		<div id="lower_block_in"></div>
+		</div>
+	<script src="/js/script.js"></script>		
+	</body>
+</html>
